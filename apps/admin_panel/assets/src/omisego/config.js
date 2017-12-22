fc7597d472f8269a680e47c025f19b9034d73fe4
mederic <mederic@omise.co>
2017-12-22 11:33:25 +0700

    Update to match latest API scheme from Kubera
    Summary: New scheme needed for latest API version
    
    Test Plan: coming.
    
    Reviewers: thibault, euro, unnawut
    
    Reviewed By: thibault, euro
    
    Maniphest Tasks: T534
    
    Differential Revision: https://phabricator.omisego.io/D325

--- apps/admin_panel/assets/src/omisego/config.js
+++ apps/admin_panel/assets/src/omisego/config.js
@@ -1,2 +1,3 @@
-export const OMISEGO_BASE_URL = "http://0.0.0.0:5000/"
-export const OMISEGO_API_KEY = "sr4zU8yA-ksZkOK0uh8gydW5zyUgGPAZZ1TbLjc-5X8"
+export const OMISEGO_BASE_URL = "http://localhost:5000/"
+export const OMISEGO_API_KEY_ID = "b64ad9c6-81b6-436d-8b17-b0a97a8b62c0"
+export const OMISEGO_API_KEY = "mzLD8PqbL3uWSB8I8Pp6servdxMz45LR_S9sKixx3R8"
--- apps/admin_panel/assets/src/omisego/helpers/auth-header.js
+++ apps/admin_panel/assets/src/omisego/helpers/auth-header.js
@@ -1,11 +1,11 @@
 import Cookies from "js-cookie";
-import { OMISEGO_API_KEY } from "../config.js"
+import { OMISEGO_API_KEY, OMISEGO_API_KEY_ID } from "../config.js"
 
 export function authHeader() {
   const authenticationToken = Cookies.get("USER-SESSION");
   if (authenticationToken) {
-    return { "Authorization": "OMGAdmin " + btoa(OMISEGO_API_KEY + ":" + authenticationToken) };
+    return { "Authorization": "OMGAdmin " + btoa(`${OMISEGO_API_KEY_ID}:${OMISEGO_API_KEY}:${authenticationToken}`) };
   } else {
-    return { "Authorization": "OMGAdmin " + btoa(OMISEGO_API_KEY) };
+    return { "Authorization": "OMGAdmin " + btoa(`${OMISEGO_API_KEY_ID}:${OMISEGO_API_KEY}`) };
   }
 }
