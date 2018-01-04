6d5a7e8cdb980ca2554d956a4932062d5c030cb4
Unnawut <unnawut@omise.co>
2018-01-04 11:37:40 +0700

    Implement admin panel login/logout
    Summary: Implement admin panel login/logout
    
    Test Plan:
    1. Run `mix ecto.migrate` (or `mix do ecto.drop, ecto.create, ecto.migrate` just to be sure)
    
    2. Seed Kubera system by running `mix seed` (or safer... `mix reset`). The seeded users and their generated passwords should be available in the seed command output.
    
    3. Then do a curl to `/login` to test:
    
    ```
    curl -X POST \
    10.5.10.20:5000/login \
    -H "Accept: application/vnd.omisego.v1+json" \
    -H "Content-Type: application/json" \
    -d '{"username": "your_username", "password": "your_password"}' \
    -v -w "\n" | jq
    ```
    
    Reviewers: mederic, thibault, sirn, euro
    
    Reviewed By: mederic, thibault, sirn
    
    Subscribers: jenkins
    
    Tags: #admin_panel, #wallet_team
    
    Maniphest Tasks: T401
    
    Differential Revision: https://phabricator.omisego.io/D258

--- apps/kubera_db/priv/repo/migrations/20171207113627_add_owner_app_to_auth_token.exs
+++ apps/kubera_db/priv/repo/migrations/20171207113627_add_owner_app_to_auth_token.exs
@@ -1,11 +1,34 @@
 defmodule KuberaDB.Repo.Migrations.AddOwnerAppToAuthTokenTable do
   use Ecto.Migration
+  import Ecto.Query
+  alias KuberaDB.Repo
 
-  def change do
+  def up do
+    # Add :owner_app without `null: false`, so existing records won't break migration.
     alter table(:auth_token) do
-      add :owner_app, :string, null: false
+      add :owner_app, :string
     end
-
     create index(:auth_token, [:owner_app])
+    flush()
+
+    # We don't know for sure which app those tokens belong to before this migration,
+    # so it's better to assign all existing tokens as :invalid_app
+    Repo.update_all("auth_token", set: [owner_app: "invalid_app"])
+
+    # Finally make :owner_app column not-nullable
+    alter table(:auth_token) do
+      modify :owner_app, :string, null: false
+    end
+  end
+
+  def down do
+    # We'll lose the scope of the token after dropping :auth_token column.
+    # Definitely not wanting a token from a specific app to be usable everywhere else.
+    Repo.update_all("auth_token", set: [expired: true])
+
+    drop index(:auth_token, [:owner_app])
+    alter table(:auth_token) do
+      remove :owner_app
+    end
   end
 end
--- apps/kubera_db/priv/repo/migrations/20171207114151_add_owner_app_to_api_key.exs
+++ apps/kubera_db/priv/repo/migrations/20171207114151_add_owner_app_to_api_key.exs
@@ -1,11 +1,34 @@
 defmodule KuberaDB.Repo.Migrations.AddOwnerAppToApiKeyTable do
   use Ecto.Migration
+  import Ecto.Query
+  alias KuberaDB.Repo
 
-  def change do
+  def up do
+    # Add :owner_app without `null: false`, so existing records won't break migration.
     alter table(:api_key) do
-      add :owner_app, :string, null: false
+      add :owner_app, :string
     end
-
     create index(:api_key, [:owner_app])
+    flush()
+
+    # We don't know for sure which app those tokens belong to before this migration,
+    # so it's better to assign all existing tokens as :invalid_app
+    Repo.update_all("api_key", set: [owner_app: "invalid_app"])
+
+    # Finally make :owner_app column not-nullable
+    alter table(:api_key) do
+      modify :owner_app, :string, null: false
+    end
+  end
+
+  def down do
+    # We'll lose the scope of the token after dropping :api_key column.
+    # Definitely not wanting a token from a specific app to be usable everywhere else.
+    Repo.update_all("api_key", set: [expired: true])
+
+    drop index(:api_key, [:owner_app])
+    alter table(:api_key) do
+      remove :owner_app
+    end
   end
 end
