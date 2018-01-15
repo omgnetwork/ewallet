4e36952aaa0bd3e645e9beaf60ac1fe70c3a79e7
Thibault <thibault@omise.co>
2018-01-15 16:21:40 +0700

    Add status check for local ledger

--- apps/kubera_api/lib/kubera_api/global/controllers/status_controller.ex
+++ apps/kubera_api/lib/kubera_api/global/controllers/status_controller.ex
@@ -1,7 +1,23 @@
 defmodule KuberaAPI.StatusController do
   use KuberaAPI, :controller
+  alias KuberaMQ.Publishers.Status
 
   def status(conn, _attrs) do
-    json conn, %{success: true}
+    json conn, %{
+      success: true,
+      services: %{
+        ewallet: true,
+        local_ledger: local_ledger()
+      }
+    }
+  end
+
+  defp local_ledger do
+    case Status.check() do
+      {:ok, _} ->
+        true
+      _ ->
+        false
+    end
   end
 end
