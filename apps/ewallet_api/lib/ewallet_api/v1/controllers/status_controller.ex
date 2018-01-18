f1c11250bd319d98474f409176f0fd0a8bb00374
Thibault <thibault@omise.co>
2018-01-18 16:06:23 +0700

    Remove status deps check

--- apps/ewallet_api/lib/ewallet_api/v1/controllers/status_controller.ex
+++ apps/ewallet_api/lib/ewallet_api/v1/controllers/status_controller.ex
@@ -1,13 +1,8 @@
 defmodule EWalletAPI.V1.StatusController do
   use EWalletAPI, :controller
-  alias EWalletMQ.Publishers.Status
 
   def index(conn, _attrs) do
     json conn, %{"success" => :true}
-  end
-
-  def status_deps(conn, _attrs) do
-    json conn, Status.check()
   end
 
   def server_error(_conn, _attrs) do
