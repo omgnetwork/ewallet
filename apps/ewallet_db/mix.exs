ea5070bdc3cbbe93e45e15670a2bb5a22d612c8e
Thibault <thibault@omise.co>
2018-01-19 15:34:35 +0700

    Move seeds into `wallet` (#19)

--- apps/ewallet_db/mix.exs
+++ apps/ewallet_db/mix.exs
@@ -55,7 +55,7 @@ defmodule EWalletDB.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       "test": ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
