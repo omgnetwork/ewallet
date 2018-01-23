ff4c285238cf25c8e51691b9d54629281f5deb10
Mederic <32560642+mederic-p@users.noreply.github.com>
2018-01-23 15:31:00 +0700

    Remove get current account mock (#26)

--- apps/admin_panel/assets/src/omisego/services/self_api.js
+++ apps/admin_panel/assets/src/omisego/services/self_api.js
@@ -5,15 +5,5 @@ export function getCurrentUser(callback) {
 }
 
 export function getCurrentAccount(callback) {
-  const mock = {
-    object: 'account',
-    created_at: '2018-01-12T09:25:09.192138Z',
-    description: 'Account 3 (Non-Master)',
-    id: '6490948c-f478-4236-ba15-64f48adbbb60',
-    master: false,
-    name: 'account03',
-    updated_at: '2018-01-12T09:25:09.192147Z',
-  };
-  callback(null, mock);
-  // return request('me.get_account', null, callback);
+  return request('me.get_account', null, callback);
 }
