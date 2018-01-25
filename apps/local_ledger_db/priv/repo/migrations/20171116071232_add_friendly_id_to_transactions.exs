24b123073915d7403b3c69a8ba3af6e476ae89d4
Thibault <thibault@omise.co>
2018-01-25 11:35:34 +0700

    Fix friendly_id migration

--- apps/local_ledger_db/priv/repo/migrations/20171116071232_add_friendly_id_to_transactions.exs
+++ apps/local_ledger_db/priv/repo/migrations/20171116071232_add_friendly_id_to_transactions.exs
@@ -1,7 +1,7 @@
 defmodule LocalLedgerDB.Repo.Migrations.AddFriendlyIDToTransactions do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:minted_token) do
       add :friendly_id, :string, null: false
     end
@@ -18,6 +18,26 @@ defmodule LocalLedgerDB.Repo.Migrations.AddFriendlyIDToTransactions do
 
     alter table(:minted_token) do
       remove :symbol
+    end
+  end
+
+  def down do
+    alter table(:minted_token) do
+      add :symbol, :string, null: false
+    end
+    create unique_index(:minted_token, [:symbol])
+
+    alter table(:transaction) do
+      remove :minted_token_friendly_id
+      add :minted_token_symbol, references(:minted_token, type: :string,
+                                                               column: :symbol),
+                                                               null: false
+    end
+
+    flush()
+
+    alter table(:minted_token) do
+      remove :friendly_id
     end
   end
 end
