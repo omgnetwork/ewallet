dd7d9059648993e34a467f49c0e2dd644fda8a72
Unnawut <unnawut@omise.co>
2018-01-25 16:58:17 +0700

    Fix /admin.all returning duplicate admins

--- apps/ewallet_db/test/ewallet_db/user_query_test.exs
+++ apps/ewallet_db/test/ewallet_db/user_query_test.exs
@@ -24,6 +24,24 @@ defmodule EWalletDB.UserQueryTest do
       refute Enum.any?(result, fn(admin) -> admin.email == user.email end)
     end
 
+    test "returns unique records" do
+      account1 = insert(:account)
+      account2 = insert(:account)
+      admin    = insert(:admin, %{email: "admin1@omise.co"})
+      role     = insert(:role, %{name: "some_role"})
+
+      insert(:membership, %{user: admin, account: account1, role: role})
+      insert(:membership, %{user: admin, account: account2, role: role})
+
+      result =
+        User
+        |> UserQuery.where_has_membership()
+        |> Repo.all()
+
+      assert Enum.count(result) == 1
+      assert Enum.at(result, 0).email == admin.email
+    end
+
     test "uses `EWalletDB.User` if `queryable` is not given" do
       account = insert(:account)
       role    = insert(:role, %{name: "some_role"})
