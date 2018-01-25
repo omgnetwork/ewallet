4082a12878911d70dcf30e58fc3a944081fdd9c8
Unnawut Leepaisalsuwanna <921194+unnawut@users.noreply.github.com>
2018-01-25 17:34:41 +0700

    Fix /admin.all returning duplicate admins (#27)
    * Fix /admin.all returning duplicate admins
    * Fix UserQueryTest should not depend on the result's ordering

--- apps/ewallet_db/lib/ewallet_db/user_query.ex
+++ apps/ewallet_db/lib/ewallet_db/user_query.ex
@@ -12,6 +12,7 @@ defmodule EWalletDB.UserQuery do
   def where_has_membership(queryable \\ User) do
     queryable
     |> join(:inner, [u], m in Membership, u.id == m.user_id)
+    |> distinct(true)
     |> select([c], c) # Returns only the User struct, not the Memberships
   end
 end
--- apps/ewallet_db/test/ewallet_db/user_query_test.exs
+++ apps/ewallet_db/test/ewallet_db/user_query_test.exs
@@ -19,9 +19,27 @@ defmodule EWalletDB.UserQueryTest do
         |> Repo.all()
 
       assert Enum.count(result) == 2
-      assert Enum.at(result, 0).email == admin1.email
-      assert Enum.at(result, 1).email == admin2.email
+      assert Enum.any?(result, fn(admin) -> admin.email == admin1.email end)
+      assert Enum.any?(result, fn(admin) -> admin.email == admin2.email end)
       refute Enum.any?(result, fn(admin) -> admin.email == user.email end)
+    end
+
+    test "returns unique records" do
+      account1 = insert(:account)
+      account2 = insert(:account)
+      admin    = insert(:admin, %{email: "admin1@omise.co"})
+      role     = insert(:role, %{name: "some_role"})
+
+      insert(:membership, %{user: admin, account: account1, role: role})
+      insert(:membership, %{user: admin, account: account2, role: role})
+
+      result =
+        User
+        |> UserQuery.where_has_membership()
+        |> Repo.all()
+
+      assert Enum.count(result) == 1
+      assert Enum.at(result, 0).email == admin.email
     end
 
     test "uses `EWalletDB.User` if `queryable` is not given" do
