9699a85036564c732fa48d373c241260bc362bbe
Thibault <thibault@omise.co>
2018-01-31 12:16:30 +0700

    Test all upload versions

--- apps/admin_api/test/admin_api/v1/controllers/account_controller_test.exs
+++ apps/admin_api/test/admin_api/v1/controllers/account_controller_test.exs
@@ -320,7 +320,14 @@ defmodule AdminAPI.V1.AccountControllerTest do
 
       assert response["success"]
       assert response["data"]["object"] == "account"
-      assert response["data"]["avatar"]["original"] =~ "http://example.com/public/uploads/test/account/avatars/#{account.id}/original.jpg?v="
+      assert response["data"]["avatar"]["large"] =~
+             "http://example.com/public/uploads/test/account/avatars/#{account.id}/large.jpg?v="
+      assert response["data"]["avatar"]["original"] =~
+             "http://example.com/public/uploads/test/account/avatars/#{account.id}/original.jpg?v="
+      assert response["data"]["avatar"]["small"] =~
+             "http://example.com/public/uploads/test/account/avatars/#{account.id}/small.jpg?v="
+      assert response["data"]["avatar"]["thumb"] =~
+             "http://example.com/public/uploads/test/account/avatars/#{account.id}/thumb.jpg?v="
     end
 
     test "returns 'account:id_not_found' if the given ID was not found" do
@@ -335,7 +342,8 @@ defmodule AdminAPI.V1.AccountControllerTest do
       refute response["success"]
       assert response["data"]["object"] == "error"
       assert response["data"]["code"] == "account:id_not_found"
-      assert response["data"]["description"] == "There is no account corresponding to the provided id"
+      assert response["data"]["description"] ==
+             "There is no account corresponding to the provided id"
     end
   end
 end
