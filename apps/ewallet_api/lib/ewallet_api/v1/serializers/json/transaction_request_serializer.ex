eb87c8d6b8b613d12e83b11514b12563102e5d0c
Thibault <thibault@omise.co>
2018-02-07 13:36:39 +0700

    Add TransactionRequest to Swagger

--- apps/ewallet_api/lib/ewallet_api/v1/serializers/json/transaction_request_serializer.ex
+++ apps/ewallet_api/lib/ewallet_api/v1/serializers/json/transaction_request_serializer.ex
@@ -11,7 +11,7 @@ defmodule EWalletAPI.V1.JSON.TransactionRequestSerializer do
       type: transaction_request.type,
       token_id: transaction_request.minted_token.friendly_id,
       amount: transaction_request.amount,
-      balance_address: transaction_request.balance_address,
+      address: transaction_request.balance_address,
       correlation_id: transaction_request.correlation_id
     }
   end
