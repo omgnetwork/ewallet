f3d1a35f9ebc77a20901ca8505b3fcfc3e65218c
Phuchit Sirimongkolsathien <phuchit@omise.co>
2018-02-08 11:45:40 +0700

    Replace loading with cute gif (#52)
    * Replace loading... with a centered cute gif
    
    * Replace loading... with a centered cute gif
    
    * Change boring loader gif
    
    * Make fun loading as a default
    
    * Add newline at the end of file _container.scss
    
    * Avoid unnecessary call

--- apps/admin_panel/assets/src/pages/startup/Startup.jsx
+++ apps/admin_panel/assets/src/pages/startup/Startup.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { getTranslate } from 'react-localize-redux';
 import Actions from './action';
+import { LOADING_GIF } from '../../../src/config';
 
 class Startup extends Component {
   static shouldLoad(newProps) {
     const { session } = newProps;
     return (!session.isSyncing && !session.isSynced);
+  }
+
+  constructor(props) {
+    super(props);
+    this.gif = LOADING_GIF.length === 1
+      ? LOADING_GIF[0]
+      : LOADING_GIF[Math.floor(Math.random() * 100) % LOADING_GIF.length];
   }
 
   componentDidMount() {
@@ -25,19 +34,25 @@ class Startup extends Component {
   }
 
   render() {
-    const { session, children } = this.props;
+    const { session, children, translate } = this.props;
     if (session.isSynced) { return children; }
     return (
-      <p>
-        Loading...
-      </p>
+      <div className="fh omg-center">
+        <div>
+          <img alt="Loading" src={this.gif} />
+          <p className="omg-center mt-1">
+            {translate('global.loading_gif_quote')}
+          </p>
+        </div>
+      </div>
     );
   }
 }
 
 function mapStateToProps(state) {
   const { session } = state;
-  return { session };
+  const translate = getTranslate(state.locale);
+  return { session, translate };
 }
 
 function mapDispatchToProps(dispatch) {
@@ -54,6 +69,7 @@ Startup.propTypes = {
   children: PropTypes.object,
   loadSession: PropTypes.func.isRequired,
   session: PropTypes.object.isRequired,
+  translate: PropTypes.func.isRequired,
 };
 
 export default connect(
