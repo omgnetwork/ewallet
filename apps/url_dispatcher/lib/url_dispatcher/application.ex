6529bb4692c0741283cadab8f01355c1f02764ad
Unnawut <unnawut@omise.co>
2018-02-09 09:57:43 +0700

    Log when UrlDispatcher is starting up

--- apps/url_dispatcher/lib/url_dispatcher/application.ex
+++ apps/url_dispatcher/lib/url_dispatcher/application.ex
@@ -1,15 +1,16 @@
 defmodule UrlDispatcher.Application do
   @moduledoc false
   use Application
+  require Logger
   alias Plug.Adapters.Cowboy
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     # List all child processes to be supervised
-    children = [
-      {Cowboy, scheme: :http, plug: UrlDispatcher.Plug, options: [port: dispatcher_port()]}
-    ]
+    children = prepare_children([
+      {:http, UrlDispatcher.Plug, port_for(:url_dispatcher)}
+    ])
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
@@ -17,12 +18,20 @@ defmodule UrlDispatcher.Application do
     Supervisor.start_link(children, opts)
   end
 
-  defp dispatcher_port do
-    :url_dispatcher
+  defp port_for(app) do
+    app
     |> Application.get_env(:port)
     |> port_to_integer()
   end
 
   defp port_to_integer(port) when is_binary(port), do: String.to_integer(port)
   defp port_to_integer(port) when is_integer(port), do: port
+
+  defp prepare_children(children) when is_list(children) do
+    Enum.map(children, &prepare_children/1)
+  end
+  defp prepare_children({scheme, plug, port}) do
+    Logger.info "Setting up #{inspect plug} with Cowboy running #{scheme} at port #{port}"
+    {Cowboy, scheme: :http, plug: plug, options: [port: port]}
+  end
 end
