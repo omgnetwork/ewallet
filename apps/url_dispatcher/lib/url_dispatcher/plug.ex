67eef501bab41de5a9bf5c5545a3727400086c34
Kridsada Thanabulpong <sirn@omise.co>
2018-02-10 04:41:53 +0700

    Redirect instead of handling the non-trailinng slash paths.

--- apps/url_dispatcher/lib/url_dispatcher/plug.ex
+++ apps/url_dispatcher/lib/url_dispatcher/plug.ex
@@ -1,7 +1,7 @@
 defmodule UrlDispatcher.Plug do
   @moduledoc false
-  import Plug.Conn, only: [resp: 3, halt: 1, send_resp: 3, put_status: 2]
-  import Phoenix.Controller, only: [json: 2]
+  import Plug.Conn, only: [resp: 3, halt: 1, put_status: 2]
+  import Phoenix.Controller, only: [json: 2, redirect: 2]
   alias Plug.Static
 
   def init(options), do: options
@@ -14,9 +14,12 @@ defmodule UrlDispatcher.Plug do
     |> halt()
   end
 
-  defp handle_request("/api", conn), do: EWalletAPI.Endpoint.call(conn, [])
+  # Redirect all endpoints without trailing slash to one with trailing slash.
+  defp handle_request("/api", conn), do: redirect(conn, to: "/api/")
+  defp handle_request("/admin/api", conn), do: redirect(conn, to: "/admin/api/")
+  defp handle_request("/public", conn), do: redirect(conn, to: "/public/")
+
   defp handle_request("/api/" <> _, conn), do: EWalletAPI.Endpoint.call(conn, [])
-  defp handle_request("/admin/api", conn), do: AdminAPI.Endpoint.call(conn, [])
   defp handle_request("/admin/api/" <> _, conn), do: AdminAPI.Endpoint.call(conn, [])
   defp handle_request("/public/" <> _, conn) do
     opts = Static.init([
@@ -30,7 +33,7 @@ defmodule UrlDispatcher.Plug do
         conn
       _ ->
         conn
-        |> send_resp(404, "The url could not be resolved.")
+        |> resp(404, "The url could not be resolved.")
         |> halt()
     end
   end
