2f4727e9a5f08e1b32ca6ceb6ee6283472701d2f
Thibault <thibault@omise.co>
2018-02-12 12:06:35 +0700

    Simplify controllers by extracting logic

--- apps/ewallet_api/lib/ewallet_api/v1/controllers/transaction_request_consumption_controller.ex
+++ apps/ewallet_api/lib/ewallet_api/v1/controllers/transaction_request_consumption_controller.ex
@@ -1,45 +1,24 @@
 defmodule EWalletAPI.V1.TransactionRequestConsumptionController do
   use EWalletAPI, :controller
   import EWalletAPI.V1.ErrorHandler
-  alias EWallet.{TransactionRequest, TransactionRequestConsumption}
-  alias EWalletDB.{User, Balance}
+  alias EWallet.Transactions.Consumption
 
-  def consume(conn, %{
-    "transaction_request_id" => request_id,
-    "correlation_id" => _,
-    "amount" => _,
-    "address" => address,
-    "metadata" => metadata
-  } = attrs) do
-    with %User{} = user <- conn.assigns.user,
-         idempotency_token <- conn.assigns[:idempotency_token],
-         request <- TransactionRequest.get(request_id) || :transaction_request_not_found,
-         %Balance{} = balance <- TransactionRequest.get_balance(user, address),
-         {:ok, consumption} <- TransactionRequestConsumption.insert(%{
-           user: user,
-           idempotency_token: idempotency_token,
-           request: request,
-           balance: balance,
-           attrs: attrs
-         }),
-         consumption <- TransactionRequestConsumption.get(consumption.id)
-    do
-      request.type
-      |> TransactionRequestConsumption.consume(consumption, metadata)
-      |> respond_with_consumption(conn)
-    else
-      error when is_atom(error) -> handle_error(conn, error)
-      {:error, changeset}       -> handle_error(conn, :invalid_parameter, changeset)
-    end
+  def consume(conn, attrs) do
+    conn.assigns.user
+    |> Consumption.consume(conn.assigns.idempotency_token, attrs)
+    |> respond(conn)
   end
 
-  defp respond_with_consumption({:ok, consumption}, conn) do
+  defp respond(error, conn) when is_atom(error), do: handle_error(conn, error)
+  defp respond({:error, changeset}, conn) do
+    handle_error(conn, :invalid_parameter, changeset)
+  end
+  defp respond({:error, code, description}, conn) do
+    handle_error(conn, code, description)
+  end
+  defp respond({:ok, consumption}, conn) do
     render(conn, :transaction_request_consumption, %{
       transaction_request_consumption: consumption
     })
-  end
-
-  defp respond_with_consumption({:error, code, description}, conn) do
-    handle_error(conn, code, description)
   end
 end
