d530c21551b23fbca4873acc1cb144b03d3c573b
Unnawut <unnawut@omise.co>
2018-02-20 00:49:49 +0700

    Add SwaggerPlug to EWalletAPI

--- apps/ewallet_api/lib/ewallet_api/router.ex
+++ apps/ewallet_api/lib/ewallet_api/router.ex
@@ -2,7 +2,10 @@ defmodule EWalletAPI.Router do
   use EWalletAPI, :router
   alias EWalletAPI.StatusController
   alias EWalletAPI.VersionedRouter
+  alias EWallet.Web.SwaggerPlug
 
   get "/api/", StatusController, :status
+
+  forward "/api/swagger", SwaggerPlug, otp_app: :ewallet_api
   forward "/api/", VersionedRouter
 end
