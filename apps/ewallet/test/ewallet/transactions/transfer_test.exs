19cfeeeea7c43452aa304eda3bde6cce12b66412
Thibault <thibault@omise.co>
2018-02-22 17:17:47 +0700

    Prevent creation of transaction where one amount equals 0 (#59)
    * Prevent creation of transaction where one amount equals 0
    
    * Split validation for Entry
    
    * Fix test fails due to transfer refactoring
    
    * Cleanup rebase (remove duplicate test)

--- apps/ewallet/test/ewallet/transactions/transfer_test.exs
+++ apps/ewallet/test/ewallet/transactions/transfer_test.exs
@@ -68,8 +68,8 @@
       {:ok, transfer} = Transactions.Transfer.get_or_insert(attrs)
       transfer = Transactions.Transfer.process(transfer)
 
-      assert transfer.status == "failed"
-      assert transfer.ledger_response["code"] == "client:insufficient_funds"
+      assert transfer.status == Transfer.failed
+      assert transfer.ledger_response["code"] == "transaction:insufficient_funds"
       assert transfer.ledger_response["description"] == "The specified balance (#{attrs[:from]})" <>
                             " does not contain enough funds. Available: 100000 #{attrs[:minted_token_friendly_id]} - Attempted debit: 1000000 #{attrs[:minted_token_friendly_id]}"
       assert transfer.status == Transfer.failed
