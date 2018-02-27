9eb54ffaec6ffadcc138adda14fefa3292247064
Thibault <thibault@omise.co>
2018-02-27 13:18:20 +0700

    Bump version to 0.1.0-beta (#80)

--- apps/ewallet_api/mix.exs
+++ apps/ewallet_api/mix.exs
@@ -4,7 +4,7 @@ defmodule EWalletAPI.Mixfile do
   def project do
     [
       app: :ewallet_api,
-      version: "0.0.1",
+      version: "0.1.0-beta",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- apps/url_dispatcher/mix.exs
+++ apps/url_dispatcher/mix.exs
@@ -4,7 +4,7 @@ defmodule UrlDispatcher.Mixfile do
   def project do
     [
       app: :url_dispatcher,
-      version: "0.1.0",
+      version: "0.1.0-beta",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
