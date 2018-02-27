358589f99cdb452d81aab8c7c0312643dc929b89
Unnawut <unnawut@omise.co>
2018-02-27 11:44:25 +0700

    Add versioning to align with open source release

--- apps/admin_panel/mix.exs
+++ apps/admin_panel/mix.exs
@@ -4,7 +4,7 @@ defmodule AdminPanel.Mixfile do
   def project do
     [
       app: :admin_panel,
-      version: "0.0.1",
+      version: "0.1.0-beta",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
