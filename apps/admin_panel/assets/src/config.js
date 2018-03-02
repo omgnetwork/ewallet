594b27e55a48364144f7aa52e1d9bdb77fc485dc
Unnawut <unnawut@omise.co>
2018-03-02 18:34:21 +0700

    Replace fun loading gif with boring ones

--- apps/admin_panel/assets/src/config.js
+++ apps/admin_panel/assets/src/config.js
@@ -1,17 +1,13 @@
 import BORING_LOADING from '../public/images/boring_loading.gif';
 
-export const FUN_LOADING_GIF = true;
+export const FUN_LOADING_GIF = false;
 export const LOADING_GIF_SRC = {
-  BORING: [
+  DEFAULT: [
     BORING_LOADING,
   ],
   FUN: [
-    'https://media.giphy.com/media/xUOwGebPearJp3Qh6o/giphy.gif',
-    'https://media.giphy.com/media/3o7abAHdYvZdBNnGZq/giphy.gif',
-    'https://media.giphy.com/media/100QWMdxQJzQC4/giphy.gif',
-    'https://media.giphy.com/media/l0Hee6sB3fXbAdhRK/giphy.gif',
-    'https://media.giphy.com/media/ypqHf6pQ5kQEg/giphy.gif',
+    // Removed third party gifs. Feel free to fun loading gifs can be added here.
   ],
 };
 
-export const LOADING_GIF = FUN_LOADING_GIF ? LOADING_GIF_SRC.FUN : LOADING_GIF_SRC.BORING;
+export const LOADING_GIF = FUN_LOADING_GIF ? LOADING_GIF_SRC.FUN : LOADING_GIF_SRC.DEFAULT;
