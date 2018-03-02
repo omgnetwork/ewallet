8a74a9c49ccd0fc111b29076fde29348fc410151
Unnawut <unnawut@omise.co>
2018-03-02 18:34:48 +0700

    Rename remaining OMISEGO_API_* envs to ADMIN_API_*

--- apps/admin_panel/assets/src/omisego/helpers/headers.js
+++ apps/admin_panel/assets/src/omisego/helpers/headers.js
@@ -1,16 +1,16 @@
 import Cookies from 'js-cookie';
 import sessionConstants from '../../constants/session.constants';
-import { OMISEGO_API_KEY, OMISEGO_API_KEY_ID } from '../config';
+import { ADMIN_API_KEY, ADMIN_API_KEY_ID } from '../config';
 import mergeHash from './helper';
 
 function authenticationHeader(authenticated) {
   const authenticationToken = Cookies.get(sessionConstants.SESSION_COOKIE);
   if (authenticated && authenticationToken) {
     return {
-      Authorization: `OMGAdmin ${btoa(`${OMISEGO_API_KEY_ID}:${OMISEGO_API_KEY}:${authenticationToken}`)}`,
+      Authorization: `OMGAdmin ${btoa(`${ADMIN_API_KEY_ID}:${ADMIN_API_KEY}:${authenticationToken}`)}`,
     };
   }
-  return { Authorization: `OMGAdmin ${btoa(`${OMISEGO_API_KEY_ID}:${OMISEGO_API_KEY}`)}` };
+  return { Authorization: `OMGAdmin ${btoa(`${ADMIN_API_KEY_ID}:${ADMIN_API_KEY}`)}` };
 }
 
 function accountHeader() {
