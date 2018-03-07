9f12e6d18beef2360a0c62bdd6d7f208cad59c12
Unnawut <unnawut@omise.co>
2018-03-07 16:10:57 +0700

    Hard code ADMIN_PANEL_BASE_DIR and ADMIN_API_BASE_URL as the values are fixed.

--- apps/admin_panel/assets/src/omisego/config.js
+++ apps/admin_panel/assets/src/omisego/config.js
@@ -1,5 +1,5 @@
-export const ADMIN_PANEL_BASE_DIR = process.env.ADMIN_PANEL_BASE_DIR;
+export const ADMIN_PANEL_BASE_DIR = "/admin";
 
-export const ADMIN_API_BASE_URL = process.env.ADMIN_API_BASE_URL;
+export const ADMIN_API_BASE_URL = "/admin/api";
 export const ADMIN_API_KEY_ID = process.env.ADMIN_API_KEY_ID;
 export const ADMIN_API_KEY = process.env.ADMIN_API_KEY;
