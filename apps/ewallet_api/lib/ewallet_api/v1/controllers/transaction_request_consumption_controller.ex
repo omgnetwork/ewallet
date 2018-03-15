bd22346d5ba2155ce9d0ddf2982fe56adea99bec
Unnawut <unnawut@omise.co>
2018-03-15 13:44:03 +0700

    Distinguish between embed (for client requests) and preload (for internal uses)

--- apps/ewallet_api/lib/ewallet_api/v1/controllers/transaction_request_consumption_controller.ex
+++ apps/ewallet_api/lib/ewallet_api/v1/controllers/transaction_request_consumption_controller.ex
@@ -4,15 +4,15 @@ defmodule EWalletAPI.V1.TransactionRequestConsumptionController do
   alias EWallet.TransactionConsumptionGate
   alias EWalletDB.Repo
 
-  # The fields that are allowed to be preloaded.
+  # The fields that are allowed to be embedded.
   # These fields must be one of the schema's association names.
-  @preloadable [:account, :minted_token, :transaction, :transaction_request, :user]
+  @embeddable [:account, :minted_token, :transaction, :transaction_request, :user]
 
-  # The fields in `@preloadable` that are preloaded regardless of the request.
+  # The fields in `@embeddable` that are embedded regardless of the request.
   # These fields must be one of the schema's association names.
-  @always_preload [:minted_token]
+  @always_embed [:minted_token]
 
-  plug :response_preload
+  plug :response_embed
 
   def consume(%{assigns: %{user: _}} = conn, attrs) do
     attrs = Map.put(attrs, "idempotency_token", conn.assigns.idempotency_token)
@@ -38,25 +38,25 @@ defmodule EWalletAPI.V1.TransactionRequestConsumptionController do
   end
   defp respond({:ok, consumption}, conn) do
     render(conn, :transaction_request_consumption, %{
-      transaction_request_consumption: preload_embed(conn, consumption)
+      transaction_request_consumption: embed(conn, consumption)
     })
   end
 
-  defp preload_embed(conn, item), do: Repo.preload(item, conn.assigns.embed)
+  defp embed(conn, item), do: Repo.preload(item, conn.assigns.embed)
 
-  def response_preload(conn, _plug_opts) do
+  def response_embed(conn, _plug_opts) do
     embeds =
       case conn.body_params["embed"] do
         embeds when is_list(embeds) -> to_existing_atoms!(embeds)
         _                           -> []
       end
 
-    # We could use `preloads -- (preloads -- preloadable)` but the complexity is O(N^3)
+    # We could use `embeds -- (embeds -- embeddable)` but the complexity is O(N^3)
     # and we're dealing with user inputs here, so it's better to convert to `MapSet`
     # before operating on the lists.
-    preloads    = MapSet.new(embeds ++ @always_preload)
-    preloadable = MapSet.new(@preloadable)
-    filtered    = MapSet.intersection(preloads, preloadable)
+    embeds     = MapSet.new(embeds ++ @always_embed)
+    embeddable = MapSet.new(@embeddable)
+    filtered   = MapSet.intersection(embeds, embeddable)
 
     case MapSet.size(filtered) do
       n when n > 0 -> assign(conn, :embed, MapSet.to_list(filtered))
