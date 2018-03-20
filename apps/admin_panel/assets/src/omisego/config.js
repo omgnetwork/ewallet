a13401e1e475287914da42fb0dadfd4a9ea8edc9
Unnawut <unnawut@omise.co>
2018-03-20 20:06:00 +0700

    Auto-inject API keys into admin panel's serving page

--- apps/admin_panel/assets/src/omisego/config.js
+++ apps/admin_panel/assets/src/omisego/config.js
@@ -1,5 +1,5 @@
 export const ADMIN_PANEL_BASE_DIR = "/admin";
 
 export const ADMIN_API_BASE_URL = "/admin/api";
-export const ADMIN_API_KEY_ID = process.env.ADMIN_API_KEY_ID;
-export const ADMIN_API_KEY = process.env.ADMIN_API_KEY;
+export const ADMIN_API_KEY_ID = window.admin_api_conf.api_key_id;
+export const ADMIN_API_KEY = window.admin_api_conf.api_key;
--- apps/admin_panel/lib/admin_panel/controllers/page_controller.ex
+++ apps/admin_panel/lib/admin_panel/controllers/page_controller.ex
@@ -1,5 +1,8 @@
 defmodule AdminPanel.PageController do
   use AdminPanel, :controller
+  import Ecto.Query
+  import EWalletDB.SoftDelete
+  alias EWalletDB.{APIKey, Repo}
   alias Plug.Conn
 
   @not_found_message """
@@ -8,22 +11,54 @@ defmodule AdminPanel.PageController do
     """
 
   def index(conn, _params) do
-    index_path =
-      case conn do
-        %{private: %{override_dist_path: dist_path}} ->
-          Path.join(dist_path, "index.html")
-        _ ->
-          dist_path = Application.get_env(:admin_panel, :dist_path)
-          Path.join(dist_path, "index.html")
-      end
+    content =
+      conn
+      |> index_file_path()
+      |> File.read!()
+      |> inject_api_key()
 
     conn
     |> put_resp_header("content-type", "text/html; charset=utf-8")
-    |> Conn.send_file(200, index_path)
+    |> Conn.send_resp(200, content)
   rescue
     File.Error ->
       conn
       |> put_resp_header("content-type", "text/plain; charset=utf-8")
       |> Conn.send_resp(:not_found, @not_found_message)
   end
+
+  defp index_file_path(%{private: %{override_dist_path: dist_path}}) do
+    Path.join(dist_path, "index.html")
+  end
+  defp index_file_path(_conn) do
+    :admin_panel
+    |> Application.get_env(:dist_path)
+    |> Path.join("index.html")
+  end
+
+  defp inject_api_key(content) do
+    String.replace(content, ~s("app"></div>), api_key_script(), insert_replaced: 0)
+  end
+
+  defp api_key_script do
+    APIKey
+    |> exclude_deleted()
+    |> limit(1)
+    |> Repo.get_by(%{owner_app: "admin_api"})
+    |> api_key_script()
+  end
+
+  defp api_key_script(%APIKey{} = api_key) do
+    """
+    <script>
+      var admin_api_conf = {};
+
+      admin_api_conf.api_key_id = "#{api_key.id}";
+      admin_api_conf.api_key = "#{api_key.key}";
+
+      window.admin_api_conf = admin_api_conf;
+    </script>
+    """
+  end
+  defp api_key_script(_not_api_key), do: ""
 end
