538bd110b49440a12484d39c5da09f11529a68fb
Unnawut Leepaisalsuwanna <921194+unnawut@users.noreply.github.com>
2018-04-24 16:31:38 +0700

    Add `mix omg.deps` to get fullstack deps (#158)

---
+++ apps/ewallet/lib/mix/tasks/omg.deps.ex
@@ -0,0 +1,36 @@
+defmodule Mix.Tasks.Omg.Deps do
+  @moduledoc """
+  Retrieve dependencies for back-end and front-end apps in one go.
+
+  ## Examples
+
+  Simply run the following command:
+
+      mix omg.deps
+  """
+  use Mix.Task
+
+  @shortdoc "Retrieve dependencies for back-end and front-end apps in one go"
+
+  def run(args) do
+    Mix.shell().info("Fetching backend depedencies...")
+    deps_backend(args)
+
+    Mix.shell().info("Fetching frontend depedencies...")
+    deps_frontend()
+  end
+
+  def deps_backend(args) do
+    Mix.Task.run("deps.get", args)
+  end
+
+  def deps_frontend do
+    System.cmd(
+      "yarn",
+      ["install", "--non-interactive", "--color=always"],
+      cd: Path.expand("../../../../admin_panel/assets/", __DIR__),
+      into: IO.stream(:stdio, :line),
+      stderr_to_stdout: true
+    )
+  end
+end
