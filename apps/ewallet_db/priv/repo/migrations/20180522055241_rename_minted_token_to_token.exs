dee17e136078870e93689fa8cbe7ce95c9a91061
Thibault <thibault@omise.co>
2018-05-22 17:05:01 +0700

    Add down for migrations

--- apps/ewallet_db/priv/repo/migrations/20180522055241_rename_minted_token_to_token.exs
+++ apps/ewallet_db/priv/repo/migrations/20180522055241_rename_minted_token_to_token.exs
@@ -1,7 +1,7 @@
 defmodule EWalletDB.Repo.Migrations.RenameMintedTokenToToken do
   use Ecto.Migration
 
-  def change do
+  def up do
     tables = %{
       mint: %{minted_token_uuid: :token_uuid},
       transfer: %{minted_token_uuid: :token_uuid},
@@ -13,7 +13,7 @@ defmodule EWalletDB.Repo.Migrations.RenameMintedTokenToToken do
     # Fix invalid constraint name for mint, transfer, transaction_request
     # {table}_minted_token_id_fkey -> {table}_minted_token_uuid_fkey
     each(tables, fn table, old_name, _new_name ->
-      regenerate_constraint(table, old_name)
+      up_regenerate_constraint(table, old_name)
     end)
 
     # Rename minted token table to token
@@ -47,6 +47,36 @@ defmodule EWalletDB.Repo.Migrations.RenameMintedTokenToToken do
     end)
   end
 
+  def down do
+    tables = %{
+      mint: %{minted_token_uuid: :token_uuid},
+      transfer: %{minted_token_uuid: :token_uuid},
+      transaction_request: %{minted_token_uuid: :token_uuid},
+      transaction_consumption: %{minted_token_uuid: :token_uuid},
+      wallet: %{minted_token_uuid: :token_uuid},
+    }
+
+    rename table(:token), to: table(:minted_token)
+
+    drop constraint(:minted_token, "token_account_uuid_fkey")
+    alter table(:minted_token) do
+      modify :account_uuid, references(:account, type: :uuid,
+                                       column: :uuid, name: "minted_token_account_id_fkey"),
+                                       null: false
+    end
+
+    each(tables, fn table, old_name, new_name ->
+      down_regenerate_constraint(table, old_name, new_name)
+    end)
+
+    drop index(:token, [:id])
+    drop index(:token, [:symbol])
+    create unique_index(:minted_token, [:id])
+    create unique_index(:minted_token, :symbol)
+    create unique_index(:minted_token, :iso_code)
+    create unique_index(:minted_token, :name)
+  end
+
   defp each(tables, func) do
     Enum.each(tables, fn {table, columns} ->
       Enum.each(columns, fn {old_name, new_name} ->
@@ -55,8 +85,9 @@ defmodule EWalletDB.Repo.Migrations.RenameMintedTokenToToken do
     end)
   end
 
-  defp regenerate_constraint(:transaction_consumption, _old_name) do
+  defp up_regenerate_constraint(:transaction_consumption, _old_name) do
     # Fix transaction_request_consumption -> transaction_consumption
+    # drop constraint(:transaction_consumption, "transaction_consumption_minted_token_id_fkey")
     drop constraint(:transaction_consumption, "transaction_request_consumption_minted_token_id_fkey")
     alter table(:transaction_consumption) do
       # constraint is now transaction_consumption_minted_token_uuid_fkey
@@ -64,19 +95,48 @@ defmodule EWalletDB.Repo.Migrations.RenameMintedTokenToToken do
                                             column: :uuid), null: false
     end
   end
-  defp regenerate_constraint(:wallet, _old_name) do
+  defp up_regenerate_constraint(:wallet, _old_name) do
     # Fix invalid constraint balance_minted_token_id_fkey -> wallet_minted_token_uuid_fkey
+    # drop constraint(:wallet, "wallet_minted_token_id_fkey")
     drop constraint(:wallet, "balance_minted_token_id_fkey")
     alter table(:wallet) do
       modify :minted_token_uuid, references(:minted_token, type: :uuid,
                                             column: :uuid), null: false
     end
   end
-  defp regenerate_constraint(table, old_name) do
+  defp up_regenerate_constraint(table, old_name) do
     drop constraint(table, "#{Atom.to_string(table)}_minted_token_id_fkey")
     alter table(table) do
       modify old_name, references(:minted_token, type: :uuid,
                                   column: :uuid), null: false
+    end
+  end
+
+  defp down_regenerate_constraint(:transaction_consumption = table, old_name, new_name) do
+    drop constraint(table, "#{Atom.to_string(table)}_#{Atom.to_string(new_name)}_fkey")
+    rename table(table), new_name, to: old_name
+    alter table(table) do
+      modify old_name, references(:minted_token, type: :uuid,
+                                  column: :uuid, name: "transaction_request_consumption_minted_token_id_fkey"),
+                                  null: false
+    end
+  end
+  defp down_regenerate_constraint(:wallet = table, old_name, new_name) do
+    drop constraint(table, "#{Atom.to_string(table)}_#{Atom.to_string(new_name)}_fkey")
+    rename table(table), new_name, to: old_name
+    alter table(table) do
+      modify old_name, references(:minted_token, type: :uuid,
+                                  column: :uuid, name: "balance_minted_token_id_fkey"),
+                                  null: false
+    end
+  end
+  defp down_regenerate_constraint(table, old_name, new_name) do
+    drop constraint(table, "#{Atom.to_string(table)}_#{Atom.to_string(new_name)}_fkey")
+    rename table(table), new_name, to: old_name
+    alter table(table) do
+      modify old_name, references(:minted_token, type: :uuid,
+                                  column: :uuid, name: "#{Atom.to_string(table)}_minted_token_id_fkey"),
+                                  null: false
     end
   end
 
--- apps/local_ledger_db/priv/repo/migrations/20180522054422_rename_minted_token_to_token.exs
+++ apps/local_ledger_db/priv/repo/migrations/20180522054422_rename_minted_token_to_token.exs
@@ -1,7 +1,7 @@
 defmodule LocalLedgerDB.Repo.Migrations.RenameMintedTokenToToken do
   use Ecto.Migration
 
-  def change do
+  def up do
     tables = %{
       transaction: %{minted_token_id: :token_id},
     }
@@ -22,6 +22,33 @@ defmodule LocalLedgerDB.Repo.Migrations.RenameMintedTokenToToken do
       Enum.each(columns, fn {_old_name, new_name} ->
         alter table(table) do
           modify new_name, references(:token, type: :string,
+                                      column: :id), null: false
+        end
+      end)
+    end)
+  end
+
+  def down do
+    tables = %{
+      transaction: %{minted_token_id: :token_id},
+    }
+
+    rename table(:token), to: table(:minted_token)
+
+    Enum.each(tables, fn {table, columns} ->
+      Enum.each(columns, fn {old_name, new_name} ->
+        rename table(table), new_name, to: old_name
+        drop constraint(table, "#{Atom.to_string(table)}_#{Atom.to_string(new_name)}_fkey")
+      end)
+    end)
+
+    drop index(:token, [:id])
+    create unique_index(:minted_token, [:id])
+
+    Enum.each(tables, fn {table, columns} ->
+      Enum.each(columns, fn {old_name, _new_name} ->
+        alter table(table) do
+          modify old_name, references(:minted_token, type: :string,
                                       column: :id), null: false
         end
       end)
