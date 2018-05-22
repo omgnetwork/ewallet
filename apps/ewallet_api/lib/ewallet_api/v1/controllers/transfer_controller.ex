4311edc894c26e2451dd697030028598deb91fee
Mederic <32560642+mederic-p@users.noreply.github.com>
2018-05-22 10:35:49 +0700

    T278 me.transfer (#195)
    * Add me.transfer endpoint
    
    * Update docs.ui
    
    * Improve error handling
    
    * Add tests
    
    * Update following github comments
    
    * Rename missing balance to wallet

--- apps/ewallet_api/lib/ewallet_api/v1/controllers/transfer_controller.ex
+++ apps/ewallet_api/lib/ewallet_api/v1/controllers/transfer_controller.ex
@@ -1,9 +1,8 @@
 defmodule EWalletAPI.V1.TransferController do
   use EWalletAPI, :controller
-  import EWalletAPI.V1.ErrorHandler
-  alias EWallet.{BalanceFetcher, TransactionGate}
-
-  plug(:put_view, EWalletAPI.V1.WalletView)
+  import EWalletAPI.V1.{ErrorHandler}
+  alias EWallet.{BalanceFetcher, TransactionGate, WalletFetcher}
+  alias EWalletAPI.V1.{WalletView, TransactionView}
 
   def transfer(
         conn,
@@ -19,10 +18,44 @@ defmodule EWalletAPI.V1.TransferController do
     attrs
     |> Map.put("idempotency_token", conn.assigns[:idempotency_token])
     |> TransactionGate.process_with_addresses()
-    |> respond_with_wallets(conn)
+    |> respond_with(:wallets, conn)
   end
 
   def transfer(conn, _attrs), do: handle_error(conn, :invalid_parameter)
+
+  def transfer_for_user(
+        conn,
+        %{
+          "to_address" => to_address,
+          "token_id" => token_id,
+          "amount" => amount
+        } = attrs
+      )
+      when to_address != nil and token_id != nil and is_integer(amount) do
+    conn.assigns.user
+    |> WalletFetcher.get(attrs["from_address"])
+    |> transfer_from_wallet(conn, attrs)
+  end
+
+  def transfer_for_user(conn, _attrs), do: handle_error(conn, :invalid_parameter)
+
+  defp transfer_from_wallet({:ok, from_wallet}, conn, attrs) do
+    attrs
+    |> Map.put("idempotency_token", conn.assigns[:idempotency_token])
+    |> Map.put("from_address", from_wallet.address)
+    |> TransactionGate.process_with_addresses()
+    |> respond_with(:transfer, conn)
+  end
+
+  defp transfer_from_wallet({:error, :wallet_not_found}, conn, _attrs) do
+    handle_error(conn, :from_address_not_found)
+  end
+
+  defp transfer_from_wallet({:error, :user_wallet_mismatch}, conn, _attrs) do
+    handle_error(conn, :from_address_mismatch)
+  end
+
+  defp transfer_from_wallet({:error, error}, conn, _attrs), do: handle_error(conn, error)
 
   def credit(conn, attrs), do: credit_or_debit(conn, TransactionGate.credit_type(), attrs)
   def debit(conn, attrs), do: credit_or_debit(conn, TransactionGate.debit_type(), attrs)
@@ -38,12 +71,18 @@ defmodule EWalletAPI.V1.TransferController do
     |> Map.put("type", type)
     |> Map.put("idempotency_token", conn.assigns[:idempotency_token])
     |> TransactionGate.process_credit_or_debit()
-    |> respond_with_wallets(conn)
+    |> respond_with(:wallets, conn)
   end
 
   defp credit_or_debit(conn, _type, _attrs), do: handle_error(conn, :invalid_parameter)
 
-  defp respond_with_wallets({:ok, _transfer, wallets, minted_token}, conn) do
+  defp respond_with({:ok, transfer, _balances, _minted_token}, :transfer, conn) do
+    conn
+    |> put_view(TransactionView)
+    |> render(:transaction, %{transaction: transfer})
+  end
+
+  defp respond_with({:ok, _transfer, wallets, minted_token}, :wallets, conn) do
     wallets =
       Enum.map(wallets, fn wallet ->
         case BalanceFetcher.get(minted_token.id, wallet.address) do
@@ -58,14 +97,16 @@ defmodule EWalletAPI.V1.TransferController do
     end
   end
 
-  defp respond_with_wallets({:error, code}, conn), do: handle_error(conn, code)
+  defp respond_with({:error, code}, _, conn), do: handle_error(conn, code)
 
-  defp respond_with_wallets({:error, _transfer, code, description}, conn) do
+  defp respond_with({:error, _transfer, code, description}, _, conn) do
     handle_error(conn, code, description)
   end
 
   defp respond({:ok, wallets}, conn) do
-    render(conn, :wallets, %{wallets: wallets})
+    conn
+    |> put_view(WalletView)
+    |> render(:wallets, %{wallets: wallets})
   end
 
   defp respond({:error, code, description}, conn) do
