3668ba1d68bf98677c24c2f4b2f1d4e3b789adca
Unnawut <unnawut@omise.co>
2018-05-23 01:42:29 +0700

    Remove uuid and timestamps from pivot table

--- apps/ewallet_db/priv/repo/migrations/20180522090048_create_category_table.exs
+++ apps/ewallet_db/priv/repo/migrations/20180522090048_create_category_table.exs
@@ -16,11 +16,8 @@ defmodule EWalletDB.Repo.Migrations.CreateCategoryTable do
 
     # Create the pivot table to support many-to-many account <-> category relationship
     create table(:account_category, primary_key: false) do
-      add :uuid, :uuid, primary_key: true
       add :account_uuid, references(:account, column: :uuid, type: :uuid)
       add :category_uuid, references(:category, column: :uuid, type: :uuid)
-
-      timestamps()
     end
 
     create unique_index(:account_category, [:account_uuid, :category_uuid])
