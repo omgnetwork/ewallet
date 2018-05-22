cefccd0d776b0362bc3a0622f8a0f1d91a318e2f
Thibault <thibault@omise.co>
2018-05-22 17:24:57 +0700

    Fix down functions for balance renaming

--- apps/local_ledger_db/priv/repo/migrations/20180514095907_rename_balance_to_wallet.exs
+++ apps/local_ledger_db/priv/repo/migrations/20180514095907_rename_balance_to_wallet.exs
@@ -1,7 +1,7 @@
 defmodule LocalLedgerDB.Repo.Migrations.RenameBalanceToWallet do
   use Ecto.Migration
 
-  def change do
+  def up do
     tables = %{
       cached_balance: %{balance_address: :wallet_address},
       transaction: %{balance_address: :wallet_address}
@@ -23,6 +23,34 @@ defmodule LocalLedgerDB.Repo.Migrations.RenameBalanceToWallet do
       Enum.each(columns, fn {_old_name, new_name} ->
         alter table(table) do
           modify new_name, references(:wallet, type: :string,
+                                      column: :address), null: false
+        end
+      end)
+    end)
+  end
+
+  def down do
+    tables = %{
+      cached_balance: %{balance_address: :wallet_address},
+      transaction: %{balance_address: :wallet_address}
+    }
+
+    rename table(:wallet), to: table(:balance)
+
+    Enum.each(tables, fn {table, columns} ->
+      Enum.each(columns, fn {old_name, new_name} ->
+        drop constraint(table, "#{Atom.to_string(table)}_#{Atom.to_string(new_name)}_fkey")
+        rename table(table), new_name, to: old_name
+      end)
+    end)
+
+    drop index(:wallet, [:address])
+    create unique_index(:balance, [:address])
+
+    Enum.each(tables, fn {table, columns} ->
+      Enum.each(columns, fn {old_name, _new_name} ->
+        alter table(table) do
+          modify old_name, references(:balance, type: :string,
                                       column: :address), null: false
         end
       end)
