5d18439ba4468279085ba817f28a9c085c57ce31
Thibault <thibault@omise.co>
2018-05-23 12:20:55 +0700

    Refactor UUIDFetcher

--- apps/ewallet/lib/ewallet/fetchers/uuid_fetcher.ex
+++ apps/ewallet/lib/ewallet/fetchers/uuid_fetcher.ex
@@ -4,36 +4,71 @@ defmodule EWallet.UUIDFetcher do
   """
   alias EWalletDB.{Account, User}
 
+  # Add more mappings if you wish to use this fetcher for other schemas.
   @mappings %{
-    account_id: Account,
-    user_id: User
+    "account_id" => Account,
+    "user_id" => User
   }
 
+  @doc """
+  Turns external IDs into internal IDs. If a record is found, it will
+  also be added to the attributes.
+
+  Example:
+
+  UUIDFetcher.replace_external_ids(%{
+    "account_id" => "some_external_id"
+  })
+
+  => %{
+    "account_uuid" => "some_internal_id",
+    "account" => %Account{ ... }
+  }
+
+  """
+  @spec replace_external_ids(map()) :: map()
   def replace_external_ids(attrs) do
     attrs
     |> Enum.map(fn {key, value} ->
-      case String.match?(key, ~r/_id$/) do
-        true ->
-          with external_id <- String.to_existing_atom(key),
-               schema <- @mappings[external_id],
-               key = String.replace(key, "_id", "_uuid") do
-            case schema.get(value) do
-              nil ->
-                {key, nil}
-
-              record ->
-                {key, record.uuid}
-            end
-          else
-            error ->
-              error
-          end
-
-        false ->
-          {key, value}
-      end
-      # rescue
+      external_id? = String.match?(key, ~r/_id$/)
+      replace_external_id(key, value, external_id: external_id?)
     end)
+    |> List.flatten()
     |> Enum.into(%{})
+  end
+
+  defp replace_external_id(key, value, external_id: true) when is_atom(key) do
+    replace_external_id(Atom.to_string(key), value, external_id: true)
+  end
+
+  defp replace_external_id(key, value, external_id: true) do
+    schema = @mappings[key]
+    uuid_key = String.replace(key, "_id", "_uuid")
+
+    load_record(schema, uuid_key, key, value)
+  end
+
+  defp replace_external_id(key, value, external_id: false), do: {key, value}
+
+  defp load_record(nil, _, key, value), do: {key, value}
+
+  defp load_record(schema, uuid_key, _key, value) do
+    value
+    |> schema.get()
+    |> extract_uuid(uuid_key)
+  end
+
+  defp extract_uuid(nil, uuid_key) do
+    [
+      {uuid_key, nil},
+      {String.replace(uuid_key, "_uuid", ""), nil}
+    ]
+  end
+
+  defp extract_uuid(record, uuid_key) do
+    [
+      {uuid_key, record.uuid},
+      {String.replace(uuid_key, "_uuid", ""), record}
+    ]
   end
 end
--- apps/ewallet/test/ewallet/fetchers/uuid_fetcher_test.exs
+++ apps/ewallet/test/ewallet/fetchers/uuid_fetcher_test.exs
@@ -1,6 +1,54 @@
 defmodule EWallet.UUIDFetcherTest do
-  use ExUnit.Case
+  use EWallet.LocalLedgerCase, async: true
+  alias EWallet.UUIDFetcher
+  alias EWalletDB.{Account, User}
 
   describe "replace_external_ids/1" do
+    test "turns multiple external IDs into internal UUIDs" do
+      user = insert(:user)
+      account = insert(:account)
+
+      attrs = %{
+        "user_id" => user.id,
+        "account_id" => account.id
+      }
+
+      res = UUIDFetcher.replace_external_ids(attrs)
+      assert res["account_uuid"] == account.uuid
+      assert %Account{} = res["account"]
+      assert res["user_uuid"] == user.uuid
+      assert %User{} = res["user"]
+    end
+
+    test "turns external IDs into internal UUIDs" do
+      account = insert(:account)
+      attrs = %{"account_id" => account.id}
+
+      res = UUIDFetcher.replace_external_ids(attrs)
+      assert res["account_uuid"] == account.uuid
+      assert %Account{} = res["account"]
+    end
+
+    test "turns external IDs into internal UUIDs if the record does not exist" do
+      attrs = %{"account_id" => "some_id"}
+
+      res = UUIDFetcher.replace_external_ids(attrs)
+      assert res["account_uuid"] == nil
+      assert res["account"] == nil
+    end
+
+    test "returns the same attributes if no external IDs is given" do
+      attrs = %{"something" => "else"}
+
+      res = UUIDFetcher.replace_external_ids(attrs)
+      assert res == %{"something" => "else"}
+    end
+
+    test "returns the same attributes if external IDs are not supported" do
+      attrs = %{"something_id" => "fake_id"}
+
+      res = UUIDFetcher.replace_external_ids(attrs)
+      assert res == %{"something_id" => "fake_id"}
+    end
   end
 end
