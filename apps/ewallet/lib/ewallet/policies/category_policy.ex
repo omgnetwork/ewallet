5cb92e9c9ce5e6dc6c359a47a09dfef6ae62344e
Unnawut <unnawut@omise.co>
2018-05-24 10:04:00 +0700

    Proper CategoryPolicy

--- apps/ewallet/lib/ewallet/policies/category_policy.ex
+++ apps/ewallet/lib/ewallet/policies/category_policy.ex
@@ -2,14 +2,23 @@ defmodule EWallet.CategoryPolicy do
   @moduledoc """
   The authorization policy for accounts.
   """
+  alias EWalletDB.User
   @behaviour Bodyguard.Policy
 
-  # Allowed by any role including none
+  # Any user can list categories
   def authorize(:all, _user_id, nil), do: true
+
+  # Any user can get a category
   def authorize(:get, _user_id, _category_id), do: true
-  def authorize(:create, _user_id, nil), do: true
-  def authorize(:update, _user_id, _category_id), do: true
-  def authorize(:delete, _user_id, _category_id), do: true
+
+  # Only users with an admin role on master account can create a category
+  def authorize(:create, user_id, nil), do: User.master_admin?(user_id)
+
+  # Only users with an admin role on master account can edit a category
+  def authorize(:update, user_id, _category_id), do: User.master_admin?(user_id)
+
+  # Only users with an admin role on master account can delete a category
+  def authorize(:delete, user_id, _category_id), do: User.master_admin?(user_id)
 
   # Catch-all: deny everything else
   def authorize(_, _, _), do: false
