f9ecf6fa1a50512ed0e5e43160d0590fcd40fc42
jarindr <jarindr23@gmail.com>
2018-06-08 14:22:05 +0700

    fix typo syntax

--- apps/admin_panel/assets/src/omg-account-category/action.js
+++ apps/admin_panel/assets/src/omg-account-category/action.js
@@ -1,17 +1,15 @@
-import  * as categoryService from '../services/categoryService'
-const createCategory = ({ name, description, accountId }) => async dispatch => {
-    try {
-      const result = await categoryService.createCategory({
-        name, description, accountId 
-      })
-      if (result.data.success) {
-        return dispatch({ type: 'CATEGORY/CREATE/SUCCESS', category: result.data.data })
-      } else {
-        return dispatch({ type: 'CATEGORY/CREATE/FAILED', error: result.data.data })
-      }
-    } catch (error) {
-      return dispatch({ type: 'CATEGORY/CREATE/FAILED', error })
+import * as categoryService from '../services/categoryService'
+export const createCategory = ({ name, description, accountId }) => async dispatch => {
+  try {
+    const result = await categoryService.createCategory({
+      name, description, accountId
+    })
+    if (result.data.success) {
+      return dispatch({ type: 'CATEGORY/CREATE/SUCCESS', category: result.data.data })
+    } else {
+      return dispatch({ type: 'CATEGORY/CREATE/FAILED', error: result.data.data })
     }
+  } catch (error) {
+    return dispatch({ type: 'CATEGORY/CREATE/FAILED', error })
   }
-  
 }
