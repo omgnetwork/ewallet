9335d445505c74ce92f5958193669d49e5a1f68a
Thibault <thibault@omise.co>
2018-06-11 12:58:12 +0700

    Ensure processes started in tests are properly terminated (#234)
    * Ensure processes started in tests are properly terminated
    
    * Fix format

--- apps/ewallet/test/ewallet/fetchers/transaction_consumption_fetcher_test.exs
+++ apps/ewallet/test/ewallet/fetchers/transaction_consumption_fetcher_test.exs
@@ -11,7 +11,12 @@ defmodule EWallet.TransactionConsumptionFetcherTest do
   alias EWalletDB.{User, TransactionConsumption}
 
   setup do
-    {:ok, _} = TestEndpoint.start_link()
+    {:ok, pid} = TestEndpoint.start_link()
+
+    on_exit(fn ->
+      ref = Process.monitor(pid)
+      assert_receive {:DOWN, ^ref, _, _, _}
+    end)
 
     token = insert(:token)
     {:ok, receiver} = :user |> params_for() |> User.insert()
