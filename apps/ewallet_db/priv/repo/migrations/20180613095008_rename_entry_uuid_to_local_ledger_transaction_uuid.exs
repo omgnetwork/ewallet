fee852aff14e739b7b6e55f2f115d8d6fc2dfcd3
Unnawut <unnawut@omise.co>
2018-06-13 18:10:45 +0700

    Add migrations

---
+++ apps/ewallet_db/priv/repo/migrations/20180613095008_rename_entry_uuid_to_local_ledger_transaction_uuid.exs
@@ -0,0 +1,11 @@
+defmodule EWalletDB.Repo.Migrations.RenameEntryUuidToLocalLedgerTransactionUuid do
+  use Ecto.Migration
+
+  def up do
+    rename table(:transaction), :entry_uuid, to: :local_ledger_transaction_uuid
+  end
+
+  def down do
+    rename table(:transaction), :local_ledger_transaction_uuid, to: :entry_uuid
+  end
+end
