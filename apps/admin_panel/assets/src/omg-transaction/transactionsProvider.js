ee3c279d84dcf1e863ab580443445d0b1ae8de98
jarindr <jarindr23@gmail.com>
2018-06-26 11:21:32 +0700

    remove selectors

--- apps/admin_panel/assets/src/omg-transaction/transactionsProvider.js
+++ apps/admin_panel/assets/src/omg-transaction/transactionsProvider.js
@@ -1,17 +1,11 @@
 import { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { selectTransactions, selectTransactionsLoadingStatus } from './selector'
 import { getTransactions } from './action'
 import { compose } from 'recompose'
 const ehance = compose(
   connect(
-    (state, props) => {
-      return {
-        transactions: selectTransactions(state, props.search),
-        transactionsLoadingStatus: selectTransactionsLoadingStatus(state)
-      }
-    },
+    null,
     { getTransactions }
   )
 )
@@ -19,9 +13,7 @@ import CONSTANT from '../constants'
 class TransactionsProvider extends Component {
   static propTypes = {
     render: PropTypes.func,
-    transactions: PropTypes.array,
     getTransactions: PropTypes.func,
-    transactionsLoadingStatus: PropTypes.string,
     search: PropTypes.string,
     page: PropTypes.number,
     perPage: PropTypes.number
