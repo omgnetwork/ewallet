841596d142c6840b9d22d20ee1b72b2bdf52c621
jarindr <jarindr23@gmail.com>
2018-06-28 19:29:29 +0700

    remove consumption wrong branch

--- apps/admin_panel/assets/src/omg-request-comsumption/requestConsumptionFetcher.js
+++
@@ -1,8 +0,0 @@
-import { createFetcher } from '../utils/createFetcher'
-import { getTokens } from './action'
-import { selectTokensLoadingStatus, selectTokensCachedQuery, selectTokensCachedQueryPagination } from './selector'
-export default createFetcher('requestConsumption', getTokens, (state, props) => ({
-  loadingStatus: selectTokensLoadingStatus(state),
-  data: selectTokensCachedQuery(state)(props.cacheKey),
-  pagination: selectTokensCachedQueryPagination(state)(props.cacheKey)
-}))
