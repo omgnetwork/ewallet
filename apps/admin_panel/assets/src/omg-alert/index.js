f18096eea231d17b23d053ea49d3a3e51be8ce22
jarindr <jarindr23@gmail.com>
2018-07-01 12:28:38 +0700

    5 s alert

--- apps/admin_panel/assets/src/omg-alert/index.js
+++ apps/admin_panel/assets/src/omg-alert/index.js
@@ -72,7 +72,7 @@ class AlertItem extends Component {
   componentDidMount = () => {
     setTimeout(() => {
       this.props.clearAlert(this.props.id)
-    }, 199999)
+    }, 5000)
   }
 
   render () {
