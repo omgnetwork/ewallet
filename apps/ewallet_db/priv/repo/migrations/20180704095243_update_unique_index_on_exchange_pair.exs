bfbd7bf34a235d71e1388b3533d9ecbbcb7cd153
Unnawut <unnawut@omise.co>
2018-07-06 00:55:45 +0700

    Drop the unnecessary `where deleted_at IS NOT NULL` index

--- apps/ewallet_db/priv/repo/migrations/20180704095243_update_unique_index_on_exchange_pair.exs
+++ apps/ewallet_db/priv/repo/migrations/20180704095243_update_unique_index_on_exchange_pair.exs
@@ -2,14 +2,12 @@ defmodule EWalletDB.Repo.Migrations.UpdateUniqueIndexOnExchangePair do
   use Ecto.Migration
 
   def up do
-    drop unique_index(:exchange_pair, [:from_token_uuid, :to_token_uuid, :deleted_at])
-    create unique_index(:exchange_pair, [:from_token_uuid, :to_token_uuid, :deleted_at], where: "deleted_at IS NOT NULL")
     create unique_index(:exchange_pair, [:from_token_uuid, :to_token_uuid], where: "deleted_at IS NULL")
+    drop unique_index(:exchange_pair, [:from_token_uuid, :to_token_uuid, :deleted_at])
   end
 
   def down do
-    drop unique_index(:exchange_pair, [:from_token_uuid, :to_token_uuid, :deleted_at], where: "deleted_at IS NOT NULL")
-    drop unique_index(:exchange_pair, [:from_token_uuid, :to_token_uuid], where: "deleted_at IS NULL")
     create unique_index(:exchange_pair, [:from_token_uuid, :to_token_uuid, :deleted_at])
+    drop unique_index(:exchange_pair, [:from_token_uuid, :to_token_uuid], where: "deleted_at IS NULL")
   end
 end
