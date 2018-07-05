d93ea5c084a26242f7e7595150595846dc2d68fe
Unnawut <unnawut@omise.co>
2018-07-06 00:50:46 +0700

    Use Ecto migration functions instead of raw SQL commands

--- apps/ewallet_db/priv/repo/migrations/20180704095243_update_unique_index_on_exchange_pair.exs
+++ apps/ewallet_db/priv/repo/migrations/20180704095243_update_unique_index_on_exchange_pair.exs
@@ -2,23 +2,14 @@ defmodule EWalletDB.Repo.Migrations.UpdateUniqueIndexOnExchangePair do
   use Ecto.Migration
 
   def up do
-    execute ~s"DROP INDEX exchange_pair_from_token_uuid_to_token_uuid_deleted_at_index"
-
-    execute ~s"CREATE UNIQUE INDEX exchange_pair_from_token_uuid_to_token_uuid_deleted_at_not_null
-               ON exchange_pair (from_token_uuid, to_token_uuid, deleted_at)
-               WHERE deleted_at IS NOT NULL"
-
-    execute ~s"CREATE UNIQUE INDEX exchange_pair_from_token_uuid_to_token_uuid_deleted_at_null
-               ON exchange_pair (from_token_uuid, to_token_uuid)
-               WHERE deleted_at IS NULL"
+    drop unique_index(:exchange_pair, [:from_token_uuid, :to_token_uuid, :deleted_at])
+    create unique_index(:exchange_pair, [:from_token_uuid, :to_token_uuid, :deleted_at], where: "deleted_at IS NOT NULL")
+    create unique_index(:exchange_pair, [:from_token_uuid, :to_token_uuid], where: "deleted_at IS NULL")
   end
 
   def down do
-    execute ~s"DROP INDEX exchange_pair_from_token_uuid_to_token_uuid_deleted_at_not_null"
-
-    execute ~s"DROP INDEX exchange_pair_from_token_uuid_to_token_uuid_deleted_at_null"
-
-    execute ~s"CREATE UNIQUE INDEX exchange_pair_from_token_uuid_to_token_uuid_deleted_at_index
-               ON exchange_pair (from_token_uuid, to_token_uuid, deleted_at)"
+    drop unique_index(:exchange_pair, [:from_token_uuid, :to_token_uuid, :deleted_at], where: "deleted_at IS NOT NULL")
+    drop unique_index(:exchange_pair, [:from_token_uuid, :to_token_uuid], where: "deleted_at IS NULL")
+    create unique_index(:exchange_pair, [:from_token_uuid, :to_token_uuid, :deleted_at])
   end
 end
