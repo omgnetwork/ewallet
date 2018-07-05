69bd24299814b8b4c3994f86417420f5759da3bc
jarindr <jarindr23@gmail.com>
2018-07-05 17:47:02 +0700

    add image and texts

--- apps/admin_panel/assets/src/omg-page-dashboard/index.js
+++ apps/admin_panel/assets/src/omg-page-dashboard/index.js
@@ -1,105 +1,62 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { Bar } from 'react-chartjs-2'
 import styled from 'styled-components'
-import { Button, Icon, RatioBar } from '../omg-uikit'
+import { Button, Icon } from '../omg-uikit'
 import TopNavigation from '../omg-page-layout/TopNavigation'
-import Section, { DetailGroup } from '../omg-page-detail-layout/DetailSection'
+import { DetailGroup } from '../omg-page-detail-layout/DetailSection'
 import CurrentAccountProvider from '../omg-account-current/currentAccountProvider'
 import moment from 'moment'
-const number = new Array(30).fill().map((x, i) => i)
-const data = {
-  labels: number,
-  datasets: [
-    {
-      label: 'Credit',
-      backgroundColor: 'rgba(0,0,0,0.7)',
-      borderWidth: 1,
-      data: [
-        65,
-        59,
-        80,
-        81,
-        56,
-        55,
-        40,
-        65,
-        59,
-        80,
-        81,
-        56,
-        55,
-        40,
-        65,
-        59,
-        80,
-        81,
-        56,
-        55,
-        40,
-        65,
-        59,
-        80,
-        81,
-        56,
-        55,
-        40
-      ],
-      stack: 'a'
-    },
-    {
-      label: 'Debit',
-      backgroundColor: '#A4C4F8',
-      borderWidth: 1,
-      data: [
-        65,
-        59,
-        80,
-        81,
-        56,
-        55,
-        40,
-        65,
-        59,
-        80,
-        81,
-        56,
-        55,
-        40,
-        65,
-        59,
-        80,
-        81,
-        56,
-        55,
-        40,
-        65,
-        59,
-        80,
-        81,
-        56,
-        55,
-        40
-      ],
-      stack: 'a'
-    }
-  ]
-}
-const ChartContainer = styled.div`
-  width: 100%;
-  margin-top: 60px;
-`
 const SectionsContainer = styled.div`
   display: flex;
 `
 const SectionContainer = styled.div`
   flex: 1 1 auto;
+  :first-child {
+    width: 250px;
+  }
+  b {
+    display: block;
+  }
+  >h5 {
+    text-transform: uppercase;
+    letter-spacing: 1px;
+  }
+
 `
-const SubTitle = styled.div`
-  font-size: 10px;
-  letter-spacing: 1px;
-  font-weight: 600;
-  color: ${props => props.theme.colors.B100};
+const IntroContainer = styled.div`
+  border-radius: 4px;
+  background-color: rgba(219,233,255,0.8);
+  box-shadow: 0 2px 10px 0 rgba(4,7,13,0.1);
+  padding: 30px;
+  h5 {
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    margin-bottom: 10px;
+  }
+  margin-bottom: 20px;
+`
+const NavigationItem = styled.div`
+  margin-bottom: 15px;
+  cursor: pointer;
+  i {
+    color: ${props => props.theme.colors.BL400};
+    font-size: 16px;
+    margin-right: 10px;
+  }
+`
+const GetStartedContainer = styled.div`
+  border: 1px solid #E4E7ED;
+  padding: 30px;
+  border-radius: 4px;
+  background-color: #FFFFFF;
+  box-shadow: 0 2px 5px 0 rgba(60,65,77,0.05);
+  background-image: url(${require('../../statics/images/Main_dashboard-01.png')});
+  background-size: 550px auto;
+  background-repeat: no-repeat;
+  background-position: top right;
+  h4 {
+    letter-spacing: 1px;
+    margin-bottom: 30px;
+  }
 `
 export default class Dashboard extends Component {
   renderExportButton = () => {
@@ -113,32 +70,33 @@ export default class Dashboard extends Component {
   renderCurrentAccountSection = ({ currentAccount }) => {
     return (
       <SectionContainer>
-        <Section title={currentAccount.name || '...'}>
-          <DetailGroup>
-            <b>Created Date:</b>{' '}
-            <span>{currentAccount.created_at ? moment(currentAccount.created_at).format('DD/MM/YYYY hh:mm:ss') : '-'}</span>
-          </DetailGroup>
-          <DetailGroup>
-            <b>ID:</b> <span>{currentAccount.id}</span>
-          </DetailGroup>
-          <DetailGroup>
-            <b>Description:</b> <span>{currentAccount.description || '-'}</span>
-          </DetailGroup>
-          <DetailGroup>
-            <b>Category:</b> <span>{_.get(currentAccount, 'categories.data[0].name', '-')}</span>
-          </DetailGroup>
-          <DetailGroup>
-            <b>Account type:</b>{' '}
-            <span>
-              {currentAccount.master === true
+        <h5>Account Details</h5>
+        <DetailGroup>
+          <b>ID:</b> <span>{currentAccount.id || '...'}</span>
+        </DetailGroup>
+        <DetailGroup>
+          <b>Description:</b> <span>{currentAccount.description || '-'}</span>
+        </DetailGroup>
+        <DetailGroup>
+          <b>Category:</b> <span>{_.get(currentAccount, 'categories.data[0].name', '-')}</span>
+        </DetailGroup>
+        <DetailGroup>
+          <b>Account type:</b>{' '}
+          <span>
+            {currentAccount.master === true
                 ? 'Master'
                 : currentAccount.master === false
                   ? 'Child'
                   : '-'}
-            </span>
-          </DetailGroup>
-        </Section>
-        {/* <Section title={'History'} /> */}
+          </span>
+        </DetailGroup>
+        <DetailGroup>
+          <b>Created Date:</b>{' '}
+          <span>{moment(currentAccount.created_at).format('DD/MM/YYYY hh:mm:ss')}</span>
+        </DetailGroup>
+        <DetailGroup>
+          <b>Last Update:</b> <span>{moment(currentAccount.updated_at).format('DD/MM/YYYY hh:mm:ss')}</span>
+        </DetailGroup>
       </SectionContainer>
     )
   }
@@ -146,49 +104,28 @@ export default class Dashboard extends Component {
     return (
       <div>
         <TopNavigation
-          // buttons={}
           title='Dashboard'
           types={false}
           secondaryAction={false}
         />
         <SectionsContainer>
           <CurrentAccountProvider render={this.renderCurrentAccountSection} />
-          {/* <SectionContainer>
-            <Section title={'Token Status'}>
-              <RatioBar
-                title='TOKEN PROPORTION'
-                rows={[
-                  { percent: 20, content: 'transaction', color: '#A4C4F8' },
-                  { percent: 80, content: 'transaction', color: 'rgba(0,0,0,0.7)' }
-                ]}
-              />
-              <ChartContainer>
-                <SubTitle>TRANSACTIONS TREND</SubTitle>
-                <Bar
-                  height={250}
-                  data={data}
-                  options={{
-                    maintainAspectRatio: false,
-                    scales: {
-                      xAxes: [
-                        {
-                          stacked: true,
-                          barPercentage: 0.7,
-                          gridLines: false
-                        }
-                      ],
-                      yAxes: [
-                        {
-                          stacked: true,
-                          gridLines: false
-                        }
-                      ]
-                    }
-                  }}
-                />
-              </ChartContainer>
-            </Section>
-          </SectionContainer> */}
+          <SectionContainer>
+            <IntroContainer>
+              <h5>This is where you will see all stats and trend of transaction within current account.</h5>
+              <p>Analytics will appear when we collect enough data.</p>
+            </IntroContainer>
+            <GetStartedContainer>
+              <h4>Let's get started</h4>
+              <NavigationItem><Icon name='Arrow-Right' /> Invite team member</NavigationItem>
+              <NavigationItem><Icon name='Arrow-Right' /> Create Account</NavigationItem>
+              <NavigationItem><Icon name='Arrow-Right' /> Create Token</NavigationItem>
+              <NavigationItem><Icon name='Arrow-Right' /> Organize Wallets</NavigationItem>
+              <NavigationItem><Icon name='Arrow-Right' /> Create Request</NavigationItem>
+              <NavigationItem><Icon name='Arrow-Right' /> Generate API</NavigationItem>
+            </GetStartedContainer>
+          </SectionContainer>
+
         </SectionsContainer>
       </div>
     )
