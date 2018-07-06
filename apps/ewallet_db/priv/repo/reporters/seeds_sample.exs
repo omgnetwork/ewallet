5cff5344e0408a6644faa2170539ba67c3edee07
Unnawut Leepaisalsuwanna <921194+unnawut@users.noreply.github.com>
2018-07-06 23:13:08 +0700

    Update mix seed output (#314)

--- apps/ewallet_db/priv/repo/reporters/seeds_sample.exs
+++ apps/ewallet_db/priv/repo/reporters/seeds_sample.exs
@@ -2,7 +2,7 @@ defmodule EWalletDB.Repo.Reporters.SeedsSampleReporter do
   def run(writer, args) do
     base_url = Application.get_env(:ewallet_db, :base_url) || "https://example.com"
     admin_api_swagger_ui_url = base_url <> "/api/admin/docs"
-    ewallet_swagger_ui_url = base_url <> "/api/client/docs"
+    ewallet_api_swagger_ui_url = base_url <> "/api/client/docs"
 
     admin_id = args[:seeded_admin_user_id]
     admin_auth_token = args[:seeded_admin_auth_token]
@@ -12,29 +12,35 @@ defmodule EWalletDB.Repo.Reporters.SeedsSampleReporter do
     ewallet_api_key = args[:seeded_ewallet_api_key]
     ewallet_auth_token = args[:seeded_ewallet_auth_token]
 
-    writer.heading("Trying out OmiseGO eWallet Server with sample data")
+    writer.heading("Populating sample data")
+
     writer.print("""
-    As you have just run the seed with `--sample` option, we have generated some credentails below
-    for you to try the endpoints easily.
+    We have seeded some sample data for you to try the eWallet server out.
+    You can play with the sample data right from the documentation by following
+    the instructions below.
 
-    If you would like to seed the database with the minimum amount needed to start
-    a production environment, we recommend running `mix seed` instead.
+    ## Try out the Admin API
 
-    ## Try eWallet API's Server endpoints
-
-    1. Browse to `#{ewallet_swagger_ui_url}`
+    1. Browse to `#{admin_api_swagger_ui_url}`
     2. Click the `Authorize` button
-    3. Use the value below for `ProviderAuth`:
+    3. Use the value below for `AdminAuth` (to authenticate you as an admin user):
+
+    ```
+    OMGAdmin #{Base.encode64(admin_id <> ":" <> admin_auth_token)}
+    ```
+
+    4. Use the value below for `ProviderAuth` (to authenticate you as a provider server):
 
     ```
     OMGProvider #{Base.encode64(ewallet_key_access <> ":" <> ewallet_key_secret)}
     ```
 
-    4. Try out Server endpoints such as /login, /user.create, etc.
+    5. See the sample data by trying out Admin API endpoints such as `/token.all`,
+    `/exchange_pair.all`, `/transaction.create`, etc. right from the documentation.
 
-    ## Try eWallet API's Client endpoints
+    ## Try out the eWallet API
 
-    1. Browse to `#{ewallet_swagger_ui_url}`
+    1. Browse to `#{ewallet_api_swagger_ui_url}`
     2. Click the `Authorize` button
     3. Use the value below for `ClientAuth`:
 
@@ -42,19 +48,8 @@ defmodule EWalletDB.Repo.Reporters.SeedsSampleReporter do
     OMGClient #{Base.encode64(ewallet_api_key <> ":" <> ewallet_auth_token)}
     ```
 
-    4. Try out Client endpoints such as /me.get, /me.list_transactions, /me.logout, etc.
-
-    ## Try Admin API's User endpoints
-
-    1. Browse to `#{admin_api_swagger_ui_url}`
-    2. Click the `Authorize` button
-    3. Use the value below for `AdminAuth`:
-
-    ```
-    OMGAdmin #{Base.encode64(admin_id <> ":" <> admin_auth_token)}
-    ```
-
-    4. Try out User endpoints such as /account.create, /account.assign_user, /access_key.create, etc.
+    4. See the sample data by trying out eWallet API endpoints such as `/me.get_wallets`,
+    `/me.get_settings`, `/me.get_transactions`, etc. right from the documentation.
     """)
   end
 end
