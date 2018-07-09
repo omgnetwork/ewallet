aeb3da8edd12eef7aee378f558b667afd96bbb44
Thibault <thibault@omise.co>
2018-07-09 15:40:59 +0700

    Fix transaction request policy

--- apps/ewallet/lib/ewallet/policies/transaction_request_policy.ex
+++ apps/ewallet/lib/ewallet/policies/transaction_request_policy.ex
@@ -3,29 +3,13 @@ defmodule EWallet.TransactionRequestPolicy do
   The authorization policy for accounts.
   """
   @behaviour Bodyguard.Policy
-  alias EWallet.{PolicyHelper, WalletPolicy}
-  alias EWalletDB.{Account, TransactionRequest, Wallet}
+  alias EWallet.WalletPolicy
+  alias EWalletDB.Wallet
 
   def authorize(:all, _admin_user_or_key, nil), do: true
 
-  def authorize(:get, _admin_user_or_key, %TransactionRequest{account_uuid: nil}) do
+  def authorize(:get, _params, _request) do
     true
-  end
-
-  def authorize(:get, %{account: account}, %TransactionRequest{account_uuid: account_uuid}) do
-    Account.descendant?(account, account_uuid)
-  end
-
-  def authorize(:get, %{key: key}, transaction_request) do
-    authorize(:get, %{account: key.account}, transaction_request)
-  end
-
-  def authorize(:get, %{admin_user: user}, %TransactionRequest{account_uuid: account_uuid}) do
-    PolicyHelper.viewer_authorize(user, account_uuid)
-  end
-
-  def authorize(:get, %{end_user: user}, %TransactionRequest{user_uuid: user_uuid}) do
-    user.uuid == user_uuid
   end
 
   # Check with the passed attributes if the current accessor can
