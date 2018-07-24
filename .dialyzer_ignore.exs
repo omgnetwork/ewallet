8f26a02ac0962dac692ad73fda248babf1a768f7
Unnawut <unnawut@omise.co>
2018-07-25 01:09:29 +0700

    Add iex and mix to Dialyzer's PLT to remove most of the ignores

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,21 +1,12 @@
 [
-  "Unknown function 'Elixir.IEx':'started?'/0",
-  "Unknown function 'Elixir.Mix':env/0",
-  "Unknown function 'Elixir.Mix':shell/0",
-  "Unknown function 'Elixir.Mix.Task':run/2",
-  "Unknown function 'Elixir.Mix.Tasks.Run':run/1",
-  "Function 'iex_running?'/0 will never be called",
-  "Function run/1 has no local return",
-  "Function configure_no_halt/1 will never be called",
-  "Callback info about the 'Elixir.Mix.Task' behaviour is not available",
-  "Function run/1 has no local return",
-  "Function deliver/1 only terminates with explicit exception",
-  "The call 'Elixir.AdminAPI.V1.ErrorHandler':handle_error(Vconn@1::any(),Verror_code@1::#{'text_body':=_, _=>_}) will never return",
-  "The pattern #{'columns':=_@2} can never match the type #{'num_rows':=non_neg_integer(), 'rows':='nil' | [tuple()]}",
-  "The pattern 'invalid' can never match the type #{'avatar':=_}",
+  # Warnings from `Arc.Ecto.Schema.cast_attachments/3` macro that we couldn't touch
+  {"apps/ewallet_db/lib/ewallet_db/account.ex", :pattern_match},
+  {"apps/ewallet_db/lib/ewallet_db/user.ex", :pattern_match},
 
-  # To fix
-  "The pattern #{'__struct__':='Elixir.Ecto.Changeset'} can never match the type #{'__struct__':='Elixir.EWalletDB.User', _=>_}",
-  "[{'validation','only_one_required'},...]) breaks the contract (t(),atom(),'Elixir.String':t(),'Elixir.Keyword':t()) -> t()",
-  "[{'validation','required_exclusive'},...]) breaks the contract (t(),atom(),'Elixir.String':t(),'Elixir.Keyword':t()) -> t()",
+  # `Account.load_accounts/1` uses `query_result.columns` which is available in `%Postgrex.Result{}`
+  # but not for other adapters. Hence not returned in typespec of `Ecto.Adapters.SQL.query/4`.
+  {"apps/ewallet_db/lib/ewallet_db/account.ex", :no_return},
+
+  # TODO: Remove extra attributes e.g. `Map.put(wallet, :account_id, account.id)` from `%Wallet{}`
+  {"apps/ewallet/lib/ewallet/gates/transaction_consumption_consumer_gate.ex", :call}
 ]
