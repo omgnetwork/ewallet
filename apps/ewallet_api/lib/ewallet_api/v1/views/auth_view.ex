3f291b7b6dc21a866bbbf4cd9d0343f8f57a2b8b
Unnawut <unnawut@omise.co>
2018-08-07 16:41:47 +0700

    Update AuthView to support user.json view

--- apps/ewallet_api/lib/ewallet_api/v1/views/auth_view.ex
+++ apps/ewallet_api/lib/ewallet_api/v1/views/auth_view.ex
@@ -1,6 +1,12 @@
 defmodule EWalletAPI.V1.AuthView do
   use EWalletAPI, :view
-  alias EWallet.Web.V1.{ResponseSerializer, UserAuthTokenSerializer}
+  alias EWallet.Web.V1.{ResponseSerializer, UserSerializer, UserAuthTokenSerializer}
+
+  def render("user.json", %{user: user}) do
+    user
+    |> UserSerializer.serialize()
+    |> ResponseSerializer.serialize(success: true)
+  end
 
   def render("auth_token.json", %{auth_token: auth_token}) do
     auth_token
--- apps/ewallet_api/test/ewallet_api/v1/views/auth_view_test.exs
+++ apps/ewallet_api/test/ewallet_api/v1/views/auth_view_test.exs
@@ -1,24 +1,37 @@
 defmodule EWalletAPI.V1.AuthViewTest do
   use EWalletAPI.ViewCase, :v1
+  alias EWallet.Web.V1.UserSerializer
   alias EWalletAPI.V1.AuthView
+  alias EWalletDB.Helpers.Preloader
 
   describe "EWalletAPI.V1.AuthView.render/2" do
-    # Potential candidate to be moved to a shared library
+    test "renders user.json with the correct structure" do
+      user = insert(:user)
+
+      expected = %{
+        version: @expected_version,
+        success: true,
+        data: UserSerializer.serialize(user)
+      }
+
+      assert AuthView.render("user.json", %{user: user}) == expected
+    end
 
     test "renders auth_token.json with correct structure" do
-      auth_token = insert(:auth_token)
+      auth_token = insert(:auth_token) |> Preloader.preload(:user)
 
       expected = %{
         version: @expected_version,
         success: true,
         data: %{
           object: "authentication_token",
-          authentication_token: auth_token.token
+          authentication_token: auth_token.token,
+          user_id: auth_token.user.id,
+          user: UserSerializer.serialize(auth_token.user)
         }
       }
 
-      attrs = %{auth_token: auth_token}
-      assert AuthView.render("auth_token.json", attrs) == expected
+      assert AuthView.render("auth_token.json", %{auth_token: auth_token}) == expected
     end
 
     test "renders empty_response.json with correct structure" do
