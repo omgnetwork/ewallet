4c71ec6db4d4a52ff873c1fa889c3e5d570798c6
Unnawut <unnawut@omise.co>
2018-08-07 16:41:19 +0700

    auth token serializers return the authenticated user along with the token

--- apps/ewallet/test/ewallet/web/v1/serializers/user_auth_token_serializer_test.exs
+++ apps/ewallet/test/ewallet/web/v1/serializers/user_auth_token_serializer_test.exs
@@ -1,11 +1,11 @@
-defmodule AdminAPI.V1.AuthTokenSerializerTest do
+defmodule EWallet.Web.V1.UserAuthTokenSerializerTest do
   use EWallet.Web.SerializerCase, :v1
-  alias AdminAPI.V1.AuthTokenSerializer
+  alias EWallet.Web.V1.UserAuthTokenSerializer
 
-  describe "AuthTokenSerializer.serialize/1" do
+  describe "serialize/1" do
     test "data contains the session token" do
       auth_token = insert(:auth_token)
-      serialized = AuthTokenSerializer.serialize(auth_token)
+      serialized = UserAuthTokenSerializer.serialize(auth_token)
 
       assert serialized.object == "authentication_token"
       assert serialized.authentication_token == auth_token.token
