5cf624b76ea6416fae822e1bbcf438e91a13181f
Unnawut <unnawut@omise.co>
2018-08-23 22:46:08 +0700

    Sets a default REDIRECT_URL_PREFIXES vaule

--- apps/ewallet/config/config.exs
+++ apps/ewallet/config/config.exs
@@ -7,7 +7,7 @@ config :ewallet,
   base_url: {:system, "BASE_URL", "http://localhost:4000"},
   sender_email: {:system, "SENDER_EMAIL", "admin@localhost"},
   max_per_page: {:system, "REQUEST_MAX_PER_PAGE", 100},
-  redirect_url_prefixes: {:system, "REDIRECT_URL_PREFIXES", ""}
+  redirect_url_prefixes: {:system, "REDIRECT_URL_PREFIXES", "http://localhost:4000"}
 
 # Configs for Bamboo emailing library
 config :ewallet, EWallet.Mailer, adapter: Bamboo.LocalAdapter
