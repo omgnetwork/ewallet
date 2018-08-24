f7fa74d5315b1dad168e0d708e2710b4aee9db48
Unnawut <unnawut@omise.co>
2018-08-24 16:20:03 +0700

    Move mix command into migration as the ewallet require the query to be run

--- apps/ewallet_db/priv/repo/migrations/20180820092452_add_is_admin_to_user.exs
+++ apps/ewallet_db/priv/repo/migrations/20180820092452_add_is_admin_to_user.exs
@@ -1,9 +1,31 @@
 defmodule EWalletDB.Repo.Migrations.AddIsAdminToUser do
   use Ecto.Migration
+  import Ecto.Query
+  alias EWalletDB.Repo
 
-  def change do
+  def up do
     alter table(:user) do
       add :is_admin, :boolean, default: false, null: false
+    end
+
+    flush()
+
+    # Get users that have membership
+    query = from(m in "membership", distinct: m.user_uuid, select: m.user_uuid)
+
+    # Update those users with memberships with `is_admin: true`
+    for user_uuid <- Repo.all(query) do
+      update_query = from(u in "user",
+                   where: u.uuid == ^user_uuid,
+                   update: [set: [is_admin: true]])
+
+      Repo.update_all(update_query, [])
+    end
+  end
+
+  def down do
+    alter table(:user) do
+      remove :is_admin
     end
   end
 end
