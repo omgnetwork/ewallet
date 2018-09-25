f2df56c4f42d461a08082f21a34c5f0e83524cec
Thibault Denizet <thibault@omise.co>
2018-09-25 16:01:26 +0700

    Fix Originator aliases

--- apps/ewallet/lib/ewallet/web/originator.ex
+++ apps/ewallet/lib/ewallet/web/originator.ex
@@ -2,14 +2,14 @@ defmodule EWallet.Web.Originator do
   @moduledoc """
   Module to extract the originator from the conn.assigns.
   """
-  import EWalletDB.{AdminUser, Key}
+  alias EWalletDB.{Key, User}
 
   @spec extract(Map.t()) :: [%Key{}]
   def extract(%{key: key}) do
     key
   end
 
-  @spec extract(Map.t()) :: [%AdminUser{}]
+  @spec extract(Map.t()) :: [%User{}]
   def extract(%{admin_user: admin_user}) do
     admin_user
   end
