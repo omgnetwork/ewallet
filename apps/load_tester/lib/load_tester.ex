c795cb93100e25e91b1e8fcb94e885d8daa1d91b
Unnawut <unnawut@omise.co>
2018-10-09 14:32:54 +0700

    Add load_tester app skeleton

---
+++ apps/load_tester/lib/load_tester.ex
@@ -0,0 +1,18 @@
+defmodule LoadTester do
+  @moduledoc """
+  Documentation for LoadTester.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LoadTester.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/load_tester/lib/load_tester/application.ex
@@ -0,0 +1,20 @@
+defmodule LoadTester.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: LoadTester.Worker.start_link(arg)
+      # {LoadTester.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: LoadTester.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
