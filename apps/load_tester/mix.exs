a6f4957d9f3418700644e58348c4013d526c3419
Unnawut <unnawut@omise.co>
2018-10-18 17:32:25 +0700

    Enable chaperon

--- apps/load_tester/mix.exs
+++ apps/load_tester/mix.exs
@@ -26,7 +26,7 @@ defmodule LoadTester.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:chaperon, "~> 0.2.3", only: :dev},
+      {:chaperon, "~> 0.2.3"},
       {:deferred_config, "~> 0.1.0"}
     ]
   end
