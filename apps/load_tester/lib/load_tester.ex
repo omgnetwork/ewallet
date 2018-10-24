caf127e63247cb704447a64443a99ba8407e82c6
Unnawut <unnawut@omise.co>
2018-10-24 16:45:52 +0700

    Remove unused function

--- apps/load_tester/lib/load_tester.ex
+++ apps/load_tester/lib/load_tester.ex
@@ -2,17 +2,4 @@ defmodule LoadTester do
   @moduledoc """
   Documentation for LoadTester.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> LoadTester.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
