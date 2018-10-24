ba33bf1a657de9567afc0d82110d351fbfa94848
Thibault Denizet <thibault@omise.co>
2018-10-24 17:29:39 +0700

    Add type specs in Config

--- apps/ewallet_config/lib/ewallet_config/config.ex
+++ apps/ewallet_config/lib/ewallet_config/config.ex
@@ -14,30 +14,68 @@ defmodule EWalletConfig.Config do
     SettingLoader
   }
 
+
+  @spec start_link(Map.t()) :: {:ok, pid()} | {:error, Atom.t()}
   def start_link(named: true) do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
+  @spec start_link() :: {:ok, pid()} | {:error, Atom.t()}
   def start_link do
     GenServer.start_link(__MODULE__, [])
   end
 
+  @spec start_link(pid()) :: :ok | {:error, Atom.t()}
   def stop(pid \\ __MODULE__) do
     GenServer.stop(pid)
   end
 
+  @spec start_link(Map.t()) :: {:ok, []}
   def init(_args) do
     {:ok, []}
   end
 
+  @spec handle_call(:get_registered_apps, Atom.t(), [Atom.t()]) :: [{Atom.t(), [Atom.t]}]
+  def handle_call(:get_registered_apps, _from, registered_apps) do
+    {:reply, registered_apps, registered_apps}
+  end
+
+  @spec handle_call(:register_and_load, Atom.t(), [Atom.t()]) :: [Atom.t()]
+  def handle_call({:register_and_load, app, settings}, _from, registered_apps) do
+    SettingLoader.load_settings(app, settings)
+    {:reply, :ok, [{app, settings} | registered_apps]}
+  end
+
+  @spec handle_call(:reloads, Atom.t(), [Atom.t()]) :: :ok
+  def handle_call(:reload, _from, registered_apps) do
+    Enum.each(registered_apps, fn {app, settings} ->
+      SettingLoader.load_settings(app, settings)
+    end)
+
+    {:reply, :ok, registered_apps}
+  end
+
+  @spec get_registered_apps(pid()) :: [{Atom.t(), [Atom.t]}]
+  def get_registered_apps(pid \\ __MODULE__) do
+    GenServer.call(pid, :get_registered_apps)
+  end
+
+  @spec register_and_load(Atom.t(), [Atom.t()]) :: [{Atom.t(), [Atom.t]}]
   def register_and_load(app, settings, pid \\ __MODULE__) do
     GenServer.call(pid, {:register_and_load, app, settings})
   end
 
+  @spec reload_config(Atom.t()) :: :ok
   def reload_config(pid \\ __MODULE__) do
-    GenServer.call(pid, :reload)
+    case Node.list() do
+      [] ->
+        GenServer.call(pid, :reload)
+      _nodes ->
+        GenServer.multi_call(pid, :reload)
+    end
   end
 
+  @spec update(String.t(), Map.t(), Atom.t()) :: {:ok, %Setting{}} | {:error, Atom.t()}
   def update(key, attrs, pid \\ __MODULE__) do
     case Setting.update(key, attrs) do
       success = {:ok, _} ->
@@ -49,31 +87,20 @@ defmodule EWalletConfig.Config do
     end
   end
 
+  @spec update_all(Map.t(), Atom.t()) :: [{:ok, %Setting{}} | {:error, Atom.t()}]
+  def update_all(attrs, pid \\ __MODULE__) do
+    res = Setting.update_all(attrs)
+    reload_config(pid)
+    res
+  end
+
+  @spec insert_all_defaults(Map.t(), Atom.t()) :: :ok
   def insert_all_defaults(opts \\ %{}, pid \\ __MODULE__) do
     :ok = Setting.insert_all_defaults(opts)
     reload_config(pid)
   end
 
-  def handle_call({:register_and_load, app, settings}, _from, registered_apps) do
-    SettingLoader.load_settings(app, settings)
-    {:reply, :ok, [{app, settings} | registered_apps]}
-  end
-
-  def handle_call(:reload, _from, registered_apps) do
-    Enum.each(registered_apps, fn {app, settings} ->
-      SettingLoader.load_settings(app, settings)
-    end)
-
-    {:reply, :ok, registered_apps}
-  end
-
-  def load(app, name, opts \\ nil)
-
-  def load(app, :file_storage, _opts) do
-    ensure_settings_existence()
-    FileStorageSettingsLoader.load(app)
-  end
-
+  @spec settings() :: [%Setting{}]
   def settings do
     Setting.all()
   end
