036a85e7884251ab66ab20904ac51e218d967109
Unnawut <unnawut@omise.co>
2018-11-14 16:31:28 +0700

    Scope down role policy to key or admin users

--- apps/ewallet/lib/ewallet/policies/role_policy.ex
+++ apps/ewallet/lib/ewallet/policies/role_policy.ex
@@ -5,16 +5,20 @@ defmodule EWallet.RolePolicy do
   @behaviour Bodyguard.Policy
   alias EWalletDB.{Account, User}
 
-  # Any user can get a role
-  def authorize(:all, _user_or_key, _role_id), do: true
-  def authorize(:get, _user_or_key, _role_id), do: true
+  # Any authenticated key or admin user can retrieve the list of roles
+  def authorize(:all, %{key: _}, _role_id), do: true
+  def authorize(:all, %{admin_user: _}, _role_id), do: true
 
-  # Only keys belonging to master account can create/update a role
+  # Any authenticated key or admin user can get a role
+  def authorize(:get, %{key: _}, _role_id), do: true
+  def authorize(:get, %{admin_user: _}, _role_id), do: true
+
+  # Only keys belonging to master account can perform all operations
   def authorize(_, %{key: key}, _role_id) do
     Account.get_master_account().uuid == key.account.uuid
   end
 
-  # Only users with an admin role on master account can create/update a role
+  # Only users with an admin role on master account can perform all operations
   def authorize(_, %{admin_user: user}, _role_id) do
     User.master_admin?(user.id)
   end
