1ba68dfb1c44793edc7be40cba4f93533b87b74f
jarindr <jarindr23@gmail.com>
2018-11-26 13:51:48 +0700

    add config service

---
+++ apps/admin_panel/assets/src/services/configurationService.js
@@ -0,0 +1,21 @@
+import { authenticatedRequest } from './apiService'
+
+export function getConfiguration ({ perPage, sort, page, matchAll, matchAny }) {
+  return authenticatedRequest({
+    path: '/category.all',
+    data: {
+      per_page: perPage,
+      sort_by: sort.by,
+      sort_dir: sort.dir,
+      match_all: matchAll,
+      match_any: matchAny
+    }
+  })
+}
+
+export function updateConfiguration (config) {
+  return authenticatedRequest({
+    path: '/category.all',
+    data: config
+  })
+}
