389cd2929be2e590524bc2d27d13292ca4850694
Unnawut Leepaisalsuwanna <921194+unnawut@users.noreply.github.com>
2018-11-27 12:58:30 +0700

    Ignore empty filters (#556)

--- apps/ewallet/lib/ewallet/web/filters/match_any_parser.ex
+++ apps/ewallet/lib/ewallet/web/filters/match_any_parser.ex
@@ -7,6 +7,8 @@ defmodule EWallet.Web.MatchAnyParser do
   @spec to_query(Ecto.Queryable.t(), map(), [atom()]) :: Ecto.Queryable.t()
   def to_query(queryable, attrs, whitelist, mappings \\ %{})
 
+  def to_query(queryable, %{"match_any" => []}, _, _), do: queryable
+
   def to_query(queryable, %{"match_any" => inputs}, whitelist, mappings) do
     MatchParser.build_query(queryable, inputs, whitelist, false, MatchAnyQuery, mappings)
   end
