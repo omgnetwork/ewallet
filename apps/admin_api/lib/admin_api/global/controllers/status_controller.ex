b6edd09265e913b09edead79c5d01a7ca947e64a
mederic <mederic@omise.co>
2018-11-28 15:41:42 +0700

    Add api version infos to status endpoints

--- apps/admin_api/lib/admin_api/global/controllers/status_controller.ex
+++ apps/admin_api/lib/admin_api/global/controllers/status_controller.ex
@@ -2,6 +2,14 @@ defmodule AdminAPI.StatusController do
   use AdminAPI, :controller
 
   def status(conn, _attrs) do
-    json(conn, %{success: true})
+    json(conn, %{success: true, api_versions: api_versions(), ewallet_version: "1.1.0"})
+  end
+
+  defp api_versions do
+    api_versions = Application.get_env(:admin_api, :api_versions)
+
+    Enum.map(api_versions, fn {key, value} ->
+      %{name: value[:name], media_type: key}
+    end)
   end
 end
