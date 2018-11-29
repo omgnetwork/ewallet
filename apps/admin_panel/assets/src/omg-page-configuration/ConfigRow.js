5c4d8d4150a77b0c6443ebc1a20b34af155de7df
jarindr <jarindr23@gmail.com>
2018-11-29 17:11:00 +0700

    fix config row

--- apps/admin_panel/assets/src/omg-page-configuration/ConfigRow.js
+++ apps/admin_panel/assets/src/omg-page-configuration/ConfigRow.js
@@ -1,30 +1,61 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { Input } from '../omg-uikit'
+import { Input, Select } from '../omg-uikit'
 const ConfigRowContainer = styled.div`
   display: flex;
+  border-bottom: ${props => (props.border ? `1px solid ${props.theme.colors.S200}` : 'none')};
 `
 const ConfigCol = styled.div`
   flex: 1 1 auto;
   padding: 20px;
   vertical-align: bottom;
+  :first-child {
+    flex: 0 1 220px;
+    padding-left: 0;
+    font-weight: 600;
+  }
+  :nth-child(2) {
+    flex: 0 1 300px;
+  }
+  :last-child {
+    padding-right: 0;
+    flex: 0 1 300px;
+  }
 `
 
 export default class ConfigRow extends Component {
   static propTypes = {
     description: PropTypes.string,
     value: PropTypes.string,
-    name: PropTypes.string
+    options: PropTypes.array,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    onChange: PropTypes.func,
+    onSelectItem: PropTypes.func,
+    border: PropTypes.bool
+  }
+
+  static defaultProps = {
+    type: 'input',
+    options: []
   }
 
   render () {
     return (
-      <ConfigRowContainer>
+      <ConfigRowContainer border={this.props.border}>
         <ConfigCol>{this.props.name}</ConfigCol>
         <ConfigCol>{this.props.description}</ConfigCol>
         <ConfigCol>
-          <Input value={this.props.value} />
+          {this.props.type === 'input' && <Input value={this.props.value} />}
+          {this.props.type === 'select' && (
+            <Select
+              value={this.props.value}
+              options={this.props.options}
+              onChange={this.props.onChange}
+              onSelectItem={this.props.onSelectItem}
+            />
+          )}
         </ConfigCol>
       </ConfigRowContainer>
     )
--- apps/admin_panel/assets/src/omg-page-configuration/index.js
+++ apps/admin_panel/assets/src/omg-page-configuration/index.js
@@ -1,120 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import TopNavigation from '../omg-page-layout/TopNavigation'
 import styled from 'styled-components'
-import { Button, Icon, Avatar } from '../omg-uikit'
 import ConfigurationsFetcher from '../omg-configuration/configurationFetcher'
 import { withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import { NameColumn } from '../omg-page-account'
-import moment from 'moment'
 import queryString from 'query-string'
 import ConfigRow from './ConfigRow'
-const TokenDetailPageContainer = styled.div`
+
+const ConfigurationPageContainer = styled.div`
   position: relative;
-  display: flex;
-  flex-direction: column;
+  padding-bottom: 150px;
+`
+
+const SubSettingContainer = styled.div`
   > div {
-    flex: 1;
-  }
-  td:first-child {
-    width: 50%;
-  }
-  td:nth-child(2),
-  td:nth-child(3) {
-    width: 25%;
+    margin-left: 25px;
+    padding: 0 20px;
+    background-color: ${props => props.theme.colors.S100};
+    border-radius: 4px;
+    border: 1px solid transparent;
+    div:first-child {
+      flex: 0 0 175px;
+    }
   }
 `
-const columns = [
-  { key: 'token', title: 'TOKEN NAME', sort: true },
-  { key: 'symbol', title: 'SYMBOL', sort: true },
-  { key: 'created', title: 'CREATED DATE', sort: true }
-]
-class TokenDetailPage extends Component {
+class ConfigurationPage extends Component {
   static propTypes = {
     match: PropTypes.object,
     history: PropTypes.object,
-    location: PropTypes.object,
-    scrollTopContentContainer: PropTypes.func
+    location: PropTypes.object
   }
   state = {
-    createTokenModalOpen: queryString.parse(this.props.location.search).createToken || false,
-    exportModalOpen: false,
-    createExchangePairModalOpen: false
+    emailAdapter: ''
   }
 
-  onClickCreateToken = () => {
-    this.setState({ createTokenModalOpen: true })
+  onSelectEmailAdapter = option => {
+    this.setState({ emailAdapter: option.value })
   }
-  onClickCreateExchangePair = () => {
-    this.setState({ createExchangePairModalOpen: true })
-  }
-  onRequestCloseCreateToken = () => {
-    this.setState({ createTokenModalOpen: false })
-  }
-  onRequestCloseCreateExchangePair = () => {
-    this.setState({ createExchangePairModalOpen: false })
-  }
-  onClickExport = () => {
-    this.setState({ exportModalOpen: true })
-  }
-  onRequestCloseExport = () => {
-    this.setState({ exportModalOpen: false })
-  }
-
-  onClickLoadMore = e => {
-    this.setState(({ loadMoreTime }) => ({ loadMoreTime: loadMoreTime + 1 }))
-  }
-
-  renderExportButton = () => {
-    return (
-      <Button size='small' styleType='ghost' onClick={this.onClickExport} key={'exports'}>
-        <Icon name='Export' />
-        <span>Export</span>
-      </Button>
-    )
-  }
-  renderTransferToken = () => {
-    return (
-      <Button size='small' styleType='secondary' onClick={this.onClickCreateToken} key={'transfer'}>
-        <span>Transfer Token</span>
-      </Button>
-    )
-  }
-  renderMintTokenButton = () => {
-    return (
-      <Button size='small' onClick={this.onClickCreateToken} key={'mint'}>
-        <Icon name='Plus' /> <span>Create Token</span>
-      </Button>
-    )
-  }
-  renderCreateExchangePairButton = () => {
-    return (
-      <Button
-        size='small'
-        styleType='secondary'
-        onClick={this.onClickCreateExchangePair}
-        key={'create pair'}
-      >
-        <span>Create Exchange Pair</span>
-      </Button>
-    )
-  }
-  rowRenderer (key, data, rows) {
-    if (key === 'created') {
-      return moment(data).format('ddd, DD/MM/YYYY hh:mm:ss')
-    }
-    if (key === 'token') {
-      return (
-        <NameColumn>
-          <Avatar image={rows.avatar} name={rows.symbol} /> <span>{data}</span>
-        </NameColumn>
-      )
-    }
-    return data
-  }
-  onClickRow = (data, index) => e => {
-    const { params } = this.props.match
-    this.props.history.push(`/${params.accountId}/tokens/${data.id}`)
+  onSelectFileStorageAdapter = option => {
+    this.setState({ fileStorageAdapter: option.value })
   }
 
   getConfiguration (configurations) {
@@ -123,9 +47,166 @@ class TokenDetailPage extends Component {
       }
     })
   }
+
+  renderFileStorageAdpter (configurations) {
+    return (
+      <Fragment>
+        <h4 style={{ marginTop: '40px' }}>File Storage Adapter</h4>
+        <ConfigRow
+          name={configurations.file_storage_adapter.key}
+          description={configurations.file_storage_adapter.description}
+          value={this.state.fileStorageAdapter || configurations.file_storage_adapter.value}
+          onSelectItem={this.onSelectFileStorageAdapter}
+          type='select'
+          options={configurations.file_storage_adapter.options.map(option => ({
+            key: option,
+            value: option
+          }))}
+        />
+        {this.state.fileStorageAdapter === 'gcs' && (
+          <SubSettingContainer>
+            <div>
+              <ConfigRow
+                name={configurations.gcs_bucket.key}
+                description={configurations.gcs_bucket.description}
+                value={configurations.gcs_bucket.value}
+              />
+              <ConfigRow
+                name={configurations.gcs_credentials.key}
+                description={configurations.gcs_credentials.description}
+                value={configurations.gcs_credentials.value}
+                border={this.state.emailAdapter !== 'gcs'}
+              />
+            </div>
+          </SubSettingContainer>
+        )}
+        {this.state.fileStorageAdapter === 'aws' && (
+          <SubSettingContainer>
+            <div>
+              <ConfigRow
+                name={configurations.aws_bucket.key}
+                description={configurations.aws_bucket.description}
+                value={configurations.aws_bucket.value}
+              />
+              <ConfigRow
+                name={configurations.aws_region.key}
+                description={configurations.aws_region.description}
+                value={configurations.aws_region.value}
+              />
+              <ConfigRow
+                name={configurations.aws_access_key_id.key}
+                description={configurations.aws_access_key_id.description}
+                value={configurations.aws_access_key_id.value}
+              />
+              <ConfigRow
+                name={configurations.aws_secret_access_key.key}
+                description={configurations.aws_secret_access_key.description}
+                value={configurations.aws_secret_access_key.value}
+                border={this.state.emailAdapter !== 'aws'}
+              />
+            </div>
+          </SubSettingContainer>
+        )}
+      </Fragment>
+    )
+  }
+  renderCacheSetting (configurations) {
+    return (
+      <Fragment>
+        <h4 style={{ marginTop: '40px' }}>Cache Setting</h4>
+        <ConfigRow
+          name={configurations.balance_caching_strategy.key}
+          description={configurations.balance_caching_strategy.description}
+          value={configurations.balance_caching_strategy.value}
+        />
+      </Fragment>
+    )
+  }
+  renderGlobalSetting (configurations) {
+    return (
+      <Fragment>
+        <h4>Global Setting</h4>
+        <ConfigRow
+          name={configurations.base_url.key}
+          description={configurations.base_url.description}
+          value={configurations.base_url.value}
+        />
+        <ConfigRow
+          name={configurations.redirect_url_prefixes.key}
+          description={configurations.redirect_url_prefixes.description}
+          value={configurations.redirect_url_prefixes.value}
+        />
+        <ConfigRow
+          name={configurations.enable_standalone.key}
+          description={configurations.enable_standalone.description}
+          value={configurations.enable_standalone.value}
+        />
+        <ConfigRow
+          name={configurations.max_per_page.key}
+          description={configurations.max_per_page.description}
+          value={configurations.max_per_page.value}
+        />
+        <ConfigRow
+          name={configurations.min_password_length.key}
+          description={configurations.min_password_length.description}
+          value={configurations.min_password_length.value}
+        />
+      </Fragment>
+    )
+  }
+  renderEmailSetting (configurations) {
+    return (
+      <Fragment>
+        <h4 style={{ marginTop: '40px' }}>Email Setting</h4>
+        <ConfigRow
+          name={configurations.sender_email.key}
+          description={configurations.sender_email.description}
+          value={configurations.sender_email.value}
+        />
+        <ConfigRow
+          name={configurations.email_adapter.key}
+          description={configurations.email_adapter.description}
+          value={this.state.emailAdapter || configurations.email_adapter.value}
+          onSelectItem={this.onSelectEmailAdapter}
+          type='select'
+          options={configurations.email_adapter.options.map(option => ({
+            key: option,
+            value: option
+          }))}
+        />
+        {this.state.emailAdapter === 'smtp' && (
+          <SubSettingContainer>
+            <div>
+              <ConfigRow
+                name={configurations.smtp_host.key}
+                description={configurations.smtp_host.description}
+                value={configurations.smtp_host.value}
+              />
+              <ConfigRow
+                name={configurations.smtp_port.key}
+                description={configurations.smtp_port.description}
+                value={configurations.smtp_port.value}
+              />
+              <ConfigRow
+                name={configurations.smtp_username.key}
+                description={configurations.smtp_username.description}
+                value={configurations.smtp_username.value}
+              />
+              <ConfigRow
+                name={configurations.smtp_password.key}
+                description={configurations.smtp_password.description}
+                value={configurations.smtp_password.value}
+                border={this.state.emailAdapter !== 'smtp'}
+              />
+            </div>
+          </SubSettingContainer>
+        )}
+      </Fragment>
+    )
+  }
   renderConfigurationPage = ({ data: configurations }) => {
     return (
-      <TokenDetailPageContainer>
+      <ConfigurationPageContainer>
         <TopNavigation
           title={'Configuration'}
           buttons={null}
@@ -134,17 +215,13 @@ class TokenDetailPage extends Component {
         />
         {!_.isEmpty(configurations) && (
           <div>
-            <h4>URl config</h4>
-            <div style={{ display: 'flex' }}>
-              <ConfigRow
-                name={configurations.enable_standalone.key}
-                description={configurations.enable_standalone.description}
-                value={configurations.enable_standalone.value}
-              />
-            </div>
+            {this.renderGlobalSetting(configurations)}
+            {this.renderEmailSetting(configurations)}
+            {this.renderCacheSetting(configurations)}
+            {this.renderFileStorageAdpter(configurations)}
           </div>
         )}
-      </TokenDetailPageContainer>
+      </ConfigurationPageContainer>
     )
   }
 
@@ -162,4 +239,4 @@ class TokenDetailPage extends Component {
   }
 }
 
-export default withRouter(TokenDetailPage)
+export default withRouter(ConfigurationPage)
