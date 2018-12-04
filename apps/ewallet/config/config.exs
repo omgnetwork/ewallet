8dca210da662fcf3210649e10e764bbc052cf479
Unnawut <unnawut@omise.co>
2018-12-04 17:51:54 +0700

    Add a new mix omg.migrate.settings task to migrate envs to database settings

--- apps/ewallet/config/config.exs
+++ apps/ewallet/config/config.exs
@@ -16,7 +16,30 @@ config :ewallet,
        {:smtp_port, :port},
        {:smtp_username, :username},
        {:smtp_password, :password}
-     ]}
-  ]
+     ]},
+  ],
+  env_migration_mapping: %{
+    "BASE_URL" => "base_url",
+    "REDIRECT_URL_PREFIXES" => "redirect_url_prefixes",
+    "ENABLE_STANDALONE" => "enable_standalone",
+    "CORS_MAX_AGE" => "CORS_MAX_AGE",
+    "CORS_ORIGIN" => "CORS_ORIGIN",
+    "BALANCE_CACHING_STRATEGY" => "balance_caching_strategy",
+    "REQUEST_MAX_PER_PAGE" => "max_per_page",
+    "MIN_PASSWORD_LENGTH" => "min_password_length",
+    "SENDER_EMAIL" => "sender_email",
+    "EMAIL_ADAPTER" => "email_adapter",
+    "SMTP_HOST" => "smtp_host",
+    "SMTP_PORT" => "smtp_port",
+    "SMTP_USERNAME" => "smtp_username",
+    "SMTP_PASSWORD" => "smtp_password",
+    "FILE_STORAGE_ADAPTER" => "file_storage_adapter",
+    "GCS_BUCKET" => "gcs_bucket",
+    "GCS_CREDENTIALS" => "gcs_credentials",
+    "AWS_BUCKET" => "aws_bucket",
+    "AWS_REGION" => "aws_region",
+    "AWS_ACCESS_KEY_ID" => "aws_access_key_id",
+    "AWS_SECRET_ACCESS_KEY" => "aws_secret_access_key"
+  }
 
 import_config "#{Mix.env()}.exs"
---
+++ apps/ewallet/lib/mix/tasks/omg.migrate.settings.ex
@@ -0,0 +1,94 @@
+defmodule Mix.Tasks.Omg.Migrate.Settings do
+  @moduledoc """
+  A temporary task that migrates the settings from the machine's
+  environment variables into the database.
+
+  ## Examples
+
+  Run the following command to migrate the settings:
+
+      mix omg.migrate.settings
+
+  You will be asked to confirm the values before the migration begins,
+  or provide flag `-y` or `--yes` to skip the confirmation.
+  """
+  use Mix.Task
+  alias EWalletConfig.Setting
+
+  @start_apps [:logger, :crypto, :ssl, :postgrex, :ecto, :cloak, :ewallet_db]
+
+  def run(_args) do
+    Enum.each(@start_apps, &Application.ensure_all_started/1)
+    Logger.configure(level: :info)
+
+    :ewallet
+    |> Application.get_env(:env_migration_mapping)
+    |> build_migration_plan()
+    |> ask_confirmation()
+    |> migrate()
+  end
+
+  defp build_migration_plan(mapping) do
+    Enum.reduce(mapping, [], fn {env_name, setting_name}, accumulator ->
+      case System.get_env(env_name) do
+        nil ->
+          accumulator
+
+        value ->
+          [{setting_name, value} | accumulator]
+      end
+    end)
+  end
+
+  defp ask_confirmation([]) do
+    _ = print_info("No settings could be found in the environment variables.")
+  end
+
+  defp ask_confirmation(migration_plan) do
+    _ = print_info("The following settings will be populated into the database:\n")
+
+    _ = Enum.each(migration_plan, fn {setting_name, value} ->
+      print_info("  - #{setting_name}: \"#{value}\"")
+    end)
+
+    confirmed? = print_confirm?("\nAre you sure to migrate these settings to the database?")
+
+    case confirmed? do
+      true -> migration_plan
+      false -> :aborted
+    end
+  end
+
+  defp migrate(:aborted) do
+    print_info("Settings migration aborted.")
+  end
+
+  defp migrate(migration_plan) do
+    _ = print_info("\nMigrating the settings to the database...\n")
+    _ = migrate_each(migration_plan)
+    _ = print_info("\nSettings migration completed. Please remove the environment variables.")
+  end
+
+  defp migrate_each([]), do: :noop
+
+  defp migrate_each([{setting_name, value} | remaining]) do
+    case Setting.update(setting_name, %{value: value}) do
+      {:ok, _setting} ->
+        print_success("  - Setting `#{setting_name}` to #{inspect(value)}... Done.")
+
+      {:error, changeset} ->
+        error_message = Enum.reduce(changeset.errors, "", fn {field, {message, _}}, acc ->
+          acc <> "`#{field}` #{message}. "
+        end)
+
+        _ = print_error("  - Setting `#{setting_name}` to #{inspect(value)}... Failed. #{error_message}")
+    end
+
+    migrate_each(remaining)
+  end
+
+  defp print_info(message), do: Mix.shell().info(message)
+  defp print_confirm?(message), do: Mix.shell().yes?(message)
+  defp print_success(message), do: Mix.shell().info([:green, :bright, message])
+  defp print_error(message), do: Mix.shell().error(message)
+end
