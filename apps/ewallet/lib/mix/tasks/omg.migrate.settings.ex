c50e099c21efb744fd17563a10426ffa457fff9e
Unnawut <unnawut@omise.co>
2018-12-06 19:39:55 +0700

    Allow mix omg.migrate.settings without manual confirmation with -y, --yes flags and etc.

--- apps/ewallet/lib/mix/tasks/omg.migrate.settings.ex
+++ apps/ewallet/lib/mix/tasks/omg.migrate.settings.ex
@@ -17,40 +17,42 @@ defmodule Mix.Tasks.Omg.Migrate.Settings do
 
   @start_apps [:logger, :crypto, :ssl, :postgrex, :ecto, :cloak, :ewallet_db]
 
-  def run(_args) do
+  def run(args) do
+    assume_yes = assume_yes?(args)
+
     Enum.each(@start_apps, &Application.ensure_all_started/1)
     Logger.configure(level: :info)
 
     :ewallet
     |> Application.get_env(:env_migration_mapping)
     |> build_migration_plan()
-    |> ask_confirmation()
+    |> ask_confirmation(assume_yes)
     |> migrate()
   end
 
   defp build_migration_plan(mapping) do
     Enum.reduce(mapping, [], fn {env_name, setting_name}, accumulator ->
       case System.get_env(env_name) do
-        nil ->
-          accumulator
-
-        value ->
-          [{setting_name, value} | accumulator]
+        nil -> accumulator
+        value -> [{setting_name, value} | accumulator]
       end
     end)
   end
 
-  defp ask_confirmation([]) do
+  defp ask_confirmation(migration_plan, assume_yes)
+
+  defp ask_confirmation([], _) do
     _ = print_info("No settings could be found in the environment variables.")
   end
 
-  defp ask_confirmation(migration_plan) do
-    _ = print_info("The following settings will be populated into the database:\n")
+  defp ask_confirmation(migration_plan, true), do: migration_plan
 
-    _ =
-      Enum.each(migration_plan, fn {setting_name, value} ->
-        print_info("  - #{setting_name}: \"#{value}\"")
-      end)
+  defp ask_confirmation(migration_plan, false) do
+    print_info("The following settings will be populated into the database:\n")
+
+    Enum.each(migration_plan, fn {setting_name, value} ->
+      print_info("  - #{setting_name}: \"#{value}\"")
+    end)
 
     confirmed? = print_confirm?("\nAre you sure to migrate these settings to the database?")
 
@@ -60,14 +62,24 @@ defmodule Mix.Tasks.Omg.Migrate.Settings do
     end
   end
 
+  defp assume_yes?([]), do: false
+
+  defp assume_yes?(["-y" | _t]), do: true
+
+  defp assume_yes?(["--yes" | _t]), do: true
+
+  defp assume_yes?(["--assume_yes" | _t]), do: true
+
+  defp assume_yes?([_ | t]), do: assume_yes?(t)
+
   defp migrate(:aborted) do
     print_info("Settings migration aborted.")
   end
 
   defp migrate(migration_plan) do
-    _ = print_info("\nMigrating the settings to the database...\n")
-    _ = migrate_each(migration_plan)
-    _ = print_info("\nSettings migration completed. Please remove the environment variables.")
+    print_info("\nMigrating the settings to the database...\n")
+    migrate_each(migration_plan)
+    print_info("\nSettings migration completed. Please remove the environment variables.")
   end
 
   defp migrate_each([]), do: :noop
@@ -83,10 +95,9 @@ defmodule Mix.Tasks.Omg.Migrate.Settings do
             acc <> "`#{field}` #{message}. "
           end)
 
-        _ =
-          print_error(
-            "  - Setting `#{setting_name}` to #{inspect(value)}... Failed. #{error_message}"
-          )
+        print_error(
+          "  - Setting `#{setting_name}` to #{inspect(value)}... Failed. #{error_message}"
+        )
     end
 
     migrate_each(remaining)
