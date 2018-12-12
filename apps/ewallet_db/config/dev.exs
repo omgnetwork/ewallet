2e55a7af24e44def0c397da619b73a2ea58f69ca
Unnawut <unnawut@omise.co>
2018-12-12 14:43:53 +0700

    Formatting

--- apps/ewallet_db/config/dev.exs
+++ apps/ewallet_db/config/dev.exs
@@ -4,4 +4,3 @@ config :ewallet_db, EWalletDB.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: {:system, "DATABASE_URL", "postgres://localhost/ewallet_dev"},
   loggers: [Appsignal.Ecto, Ecto.LogEntry]
-
