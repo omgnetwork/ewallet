a5c8e0b3e9933dacb9638f596be781f27770b29d
Unnawut <unnawut@omise.co>
2018-12-15 02:40:18 +0700

    Add tests to cover all of EWallet.Web.V1.ErrorHandler

--- apps/ewallet/test/ewallet/web/v1/error_handler_test.exs
+++ apps/ewallet/test/ewallet/web/v1/error_handler_test.exs
@@ -1,22 +1,133 @@
 defmodule EWallet.Web.V1.ErrorHandlerTest do
   # async: false due to `Application.put_env/3` for sentry reporting
-  use ExUnit.Case, async: false
+  use EWallet.DBCase, async: false
   alias EWallet.Web.V1.ErrorHandler
+  alias Ecto.Changeset
   alias Plug.Conn
 
-  @errors %{
-    error_code_one: %{
-      code: "error:code_one",
-      description: "This is error code #1."
-    },
-    internal_server_error: %{
-      code: "server:internal_server_error",
-      description: "Something went wrong on the server."
-    }
-  }
-
   describe "build_error/3" do
+    test "returns an error object when given an error code and changeset" do
+      errors = %{
+        error_code: %{
+          code: "error:error_code",
+          description: "Error description."
+        }
+      }
+
+      data = %{}
+      types = %{name: :string}
+      changeset =
+        {data, types}
+        |> Changeset.change()
+        |> Changeset.validate_required(:name)
+
+      expected = %{
+        object: "error",
+        code: errors[:error_code].code,
+        description: errors[:error_code].description <> " `name` can't be blank.",
+        messages: %{"name" => [:required]}
+      }
+
+      assert ErrorHandler.build_error(:error_code, changeset, errors) == expected
+    end
+
+    test "returns an error object when given a valid code and an arbitary description" do
+      errors = %{
+        error_code: %{
+          code: "error:error_code",
+          description: "Error description."
+        }
+      }
+
+      description = "arbitary_description"
+
+      expected = %{
+        object: "error",
+        code: errors[:error_code].code,
+        description: description,
+        messages: nil
+      }
+
+      assert ErrorHandler.build_error(:error_code, description, errors) == expected
+    end
+
+    test "handles insufficient funds error" do
+      errors = %{
+        error_code: %{
+          code: "error:error_code",
+          template:
+            "The specified wallet (%{address}) does not contain enough funds. " <>
+              "Available: %{current_amount} %{token_id} - Attempted debit: " <>
+              "%{amount_to_debit} %{token_id}"
+        }
+      }
+
+      token = insert(:token)
+      data = %{
+        "address" => "some_wallet_address",
+        "current_amount" => 1 * token.subunit_to_unit,
+        "amount_to_debit" => 10 * token.subunit_to_unit,
+        "token_id" => token.id
+      }
+
+      expected = %{
+        object: "error",
+        code: errors[:error_code].code,
+        description:
+          "The specified wallet (#{data["address"]}) does not contain enough funds. " <>
+          "Available: 1 #{token.id} - Attempted debit: 10 #{token.id}",
+        messages: nil
+      }
+
+      assert ErrorHandler.build_error(:error_code, data, errors) == expected
+    end
+
+    test "returns an error object when given a valid code and templating data" do
+      errors = %{
+        error_code: %{
+          code: "error:error_code",
+          template: "Error template. Value is: '%{value}'."
+        }
+      }
+
+      expected = %{
+        object: "error",
+        code: errors[:error_code].code,
+        description: "Error template. Value is: 'ABCD'.",
+        messages: nil
+      }
+
+      assert ErrorHandler.build_error(:error_code, %{"value" => "ABCD"}, errors) == expected
+    end
+
+    test "returns an error object when given a valid code" do
+      errors = %{
+        error_code: %{
+          code: "error:error_code",
+          description: "Error description."
+        }
+      }
+
+      expected = %{
+        object: "error",
+        code: errors[:error_code].code,
+        description: "Error description.",
+        messages: nil
+      }
+
+      assert ErrorHandler.build_error(:error_code, errors) == expected
+    end
+  end
+
+  describe "build_error/2" do
     test "sends a report to sentry when the given error code could not be found" do
+      errors = %{
+        internal_server_error: %{
+          code: "server:internal_server_error",
+          description: "Something went wrong on the server."
+        }
+      }
+
       bypass = Bypass.open()
 
       Bypass.expect(bypass, fn conn ->
@@ -33,7 +144,14 @@ defmodule EWallet.Web.V1.ErrorHandlerTest do
       Application.put_env(:sentry, :dsn, "http://public@localhost:#{bypass.port}/1")
       Application.put_env(:sentry, :included_environments, [:test | original_included_envs])
 
-      ErrorHandler.build_error(:unknown_code, @errors)
+      expected = %{
+        object: "error",
+        code: errors[:internal_server_error].code,
+        description: "Something went wrong on the server.",
+        messages: nil
+      }
+
+      assert ErrorHandler.build_error(:unknown_code, errors) == expected
 
       # Because Bypass takes some time to serve the endpoint and Sentry uses
       # `Task.Supervisor.async_nolink/3` deep inside its code, the only way
