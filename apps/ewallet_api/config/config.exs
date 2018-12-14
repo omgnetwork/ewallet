6a7c84ab4c4c60a4a90d83c7081dc93e7ce69bbe
Unnawut <unnawut@omise.co>
2018-12-15 02:51:37 +0700

    Formatting

--- apps/ewallet_api/config/config.exs
+++ apps/ewallet_api/config/config.exs
@@ -72,7 +72,7 @@ config :sentry,
     env: Mix.env(),
     application: Mix.Project.config()[:app]
   },
-  server_name: (:inet.gethostname() |> elem(1) |> to_string()),
+  server_name: :inet.gethostname() |> elem(1) |> to_string(),
   included_environments: [:prod]
 
 # Import environment specific config. This must remain at the bottom
