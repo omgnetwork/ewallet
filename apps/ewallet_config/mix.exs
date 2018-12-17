9c8d9b3e049e3e4c2669dc4561eaf72913c8491b
mederic <mederic@omise.co>
2018-12-17 11:21:01 +0700

    Make sub-app version explicit in mix.exs

--- apps/ewallet_config/mix.exs
+++ apps/ewallet_config/mix.exs
@@ -4,7 +4,7 @@ defmodule EWalletConfig.MixProject do
   def project do
     [
       app: :ewallet_config,
-      version: Application.get_env(:ewallet, :version),
+      version: "1.1.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
