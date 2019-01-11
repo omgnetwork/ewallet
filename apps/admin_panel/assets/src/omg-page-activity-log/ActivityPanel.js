5de53beb3f6e6bdae94926e97d1d0c9501222e3b
jarindr <jarindr23@gmail.com>
2019-01-11 16:57:05 +0700

    add activity panel

--- apps/admin_panel/assets/src/omg-page-activity-log/ActivityPanel.js
+++ apps/admin_panel/assets/src/omg-page-activity-log/ActivityPanel.js
@@ -7,6 +7,7 @@ import { withRouter } from 'react-router-dom'
 import queryString from 'query-string'
 import { compose } from 'recompose'
 import moment from 'moment'
+import Link from '../omg-links'
 const PanelContainer = styled.div`
   height: 100vh;
   position: fixed;
@@ -15,6 +16,7 @@ const PanelContainer = styled.div`
   background-color: white;
   padding: 40px 30px;
   box-shadow: 0 0 15px 0 rgba(4, 7, 13, 0.1);
+  overflow: scroll;
   > i {
     position: absolute;
     right: 25px;
@@ -22,17 +24,49 @@ const PanelContainer = styled.div`
     top: 25px;
     cursor: pointer;
   }
+  h5 {
+    margin-bottom: 10px;
+    padding-top: 25px;
+    letter-spacing: 1px;
+    background-color: ${props => props.theme.colors.S300};
+    display: inline-block;
+    padding: 5px 10px;
+  }
 `
 const SubDetailTitle = styled.div`
   margin-top: 10px;
   color: ${props => props.theme.colors.B100};
-  margin-bottom: 10px;
+  padding-bottom: 25px;
+  margin-bottom: 25px;
+  border-bottom: 1px solid ${props => props.theme.colors.S400};
   > span {
     padding: 0 5px;
     :first-child {
       padding-left: 0;
     }
   }
+`
+
+const InformationItem = styled.div`
+  color: ${props => props.theme.colors.B200};
+  display: flex;
+  :not(:last-child) {
+    margin-bottom: 10px;
+  }
+  b {
+    white-space: nowrap;
+  }
+  span {
+    word-break: break-word;
+  }
+  .colon {
+    padding: 0 5px;
+  }
+`
+const ChangesContainer = styled.div`
+  border-top: 1px solid ${props => props.theme.colors.S400};
+  padding-top: 25px;
+  margin-top: 25px;
 `
 
 const enhance = compose(withRouter)
@@ -54,22 +88,108 @@ class TransactionRequestPanel extends Component {
       search: queryString.stringify(searchObject)
     })
   }
+  getLink (type, id) {
+    switch (type) {
+      case 'wallet':
+        return <Link to={`/wallets/${id}`}>{id}</Link>
+      case 'account':
+        return <Link to={`/accounts/${id}`}>{id}</Link>
+      case 'user':
+        return <Link to={`/users/${id}`}>{id}</Link>
+      case 'token':
+        return <Link to={`/tokens/${id}`}>{id}</Link>
+      case 'transaction':
+        const query = {
+          ...queryString.parse(this.props.location.search),
+          'show-transaction-tab': id
+        }
+        return (
+          <Link
+            to={{
+              search: queryString.stringify(query)
+            }}
+          >
+            {id}
+          </Link>
+        )
+      default:
+        return id
+    }
+  }
   render () {
     return (
       <ActivityLogProvider
         activityId={queryString.parse(this.props.location.search)['show-activity-tab']}
         render={({ activity }) => {
-          console.log(activity)
-          return (
+          return activity ? (
             <PanelContainer>
               <Icon name='Close' onClick={this.onClickClose} />
-              <h4>{activity.originator_type} | {activity.action} | {moment(activity.created_at).format('ddd, DD/MM/YYYY hh:mm:ss')}</h4>
+              <h4>
+                {activity.originator_type} | {activity.action} |{' '}
+                {moment(activity.created_at).format('ddd, DD/MM/YYYY hh:mm:ss')}
+              </h4>
               <SubDetailTitle>
                 <span>{activity.id}</span> | <span>{activity.action}</span>
               </SubDetailTitle>
-              {JSON.stringify(activity.target_changes)}
+              <div>
+                <InformationItem>
+                  <b> Originator</b>
+                  <span className='colon'> : </span>
+                  <span>
+                    {activity.originator
+                      ? this.getLink(
+                          activity.originator_type,
+                          activity.originator.id || activity.originator.address
+                        )
+                      : '-'}
+                  </span>
+                </InformationItem>
+                <InformationItem>
+                  <b>Originator Type</b>
+                  <span className='colon'> : </span>
+                  <span>{activity.originator_type}</span>
+                </InformationItem>
+                <InformationItem>
+                  <b>Action</b>
+                  <span className='colon'> : </span>
+                  <span>{activity.action}</span>
+                </InformationItem>
+                <InformationItem>
+                  <b>Target</b>
+                  <span className='colon'> : </span>
+                  <span>
+                    {' '}
+                    {activity.target
+                      ? this.getLink(
+                          activity.target_type,
+                          activity.target.id || activity.target.address
+                        )
+                      : '-'}
+                  </span>
+                </InformationItem>
+                <InformationItem>
+                  <b>Target Type</b>
+                  <span className='colon'> : </span>
+                  <span>{activity.target_type}</span>
+                </InformationItem>
+                <InformationItem>
+                  <b>Timestamp</b>
+                  <span className='colon'> : </span>
+                  <span>{moment(activity.created_at).format('ddd, DD/MM/YYYY hh:mm:ss')}</span>
+                </InformationItem>
+              </div>
+              <ChangesContainer>
+                <h5>Changes</h5>
+                {Object.keys(activity.target_changes).map(key => (
+                  <InformationItem key={key}>
+                    <b> {_.startCase(_.toLower(key))}</b>{' '}
+                    <span className='colon'> : </span>
+                    <span>{JSON.stringify(activity.target_changes[key]).replace(/"/g, '')}</span>
+                  </InformationItem>
+                ))}
+              </ChangesContainer>
             </PanelContainer>
-          )
+          ) : null
         }}
       />
     )
