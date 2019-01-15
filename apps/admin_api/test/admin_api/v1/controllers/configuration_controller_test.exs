14ee18fc2a3bb15fbaaded18c1d845b2bd6c4919
mederic <mederic@omise.co>
2019-01-15 15:49:21 +0700

    Merge configuration controller test [NOT WORKING]

--- apps/admin_api/test/admin_api/v1/controllers/configuration_controller_test.exs
+++ apps/admin_api/test/admin_api/v1/controllers/configuration_controller_test.exs
@@ -12,26 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-defmodule AdminAPI.V1.ProviderAuth.ConfigurationControllerTest do
+defmodule AdminAPI.V1.ConfigurationControllerTest do
   use AdminAPI.ConnCase, async: true
   alias EWalletConfig.{Config, StoredSetting}
 
-  describe "/configuration.get" do
-    test "returns a list of settings" do
-      response = provider_request("/configuration.all", %{})
-      default_settings = Application.get_env(:ewallet_config, :default_settings)
+  describe "/configuration.all" do
+    test_with_auths "returns a list of configurations" do
+      response = request("/configuration.all", %{})
+
       # Asserts return data
       assert response["success"]
       assert response["data"]["object"] == "list"
       assert is_list(response["data"]["data"])
-      assert length(response["data"]["data"]) == Enum.count(default_settings)
+    end
+
+    test_with_auths "returns a list of settings" do
+      response = request("/configuration.all")
+      assert response["success"] == true
+
+      assert length(response["data"]["data"]) ==
+               Enum.count(Application.get_env(:ewallet_config, :default_settings))
     end
   end
 
   describe "/configuration.update" do
-    test "updates one setting", meta do
+    test_with_auths "updates one setting" do
+      # IO.inspect(meta)
       response =
-        provider_request("/configuration.update", %{
+        request("/configuration.update", %{
           base_url: "new_base_url.example",
           config_pid: meta[:config_pid]
         })
@@ -41,13 +49,14 @@ defmodule AdminAPI.V1.ProviderAuth.ConfigurationControllerTest do
       assert response["data"]["data"]["base_url"]["value"] == "new_base_url.example"
     end
 
-    test "updates a list of settings", meta do
+    test_with_auths "updates a list of settings" do
       response =
-        provider_request("/configuration.update", %{
+        request("/configuration.update", %{
           "aws_access_key_id" => "asd",
           "aws_bucket" => "asd",
           "aws_region" => "asdz",
           "aws_secret_access_key" => "asdasdasdasdasd",
+          "enable_standalone" => false,
           "config_pid" => meta[:config_pid]
         })
 
@@ -58,16 +67,18 @@ defmodule AdminAPI.V1.ProviderAuth.ConfigurationControllerTest do
       assert data["aws_bucket"]["value"] == "asd"
       assert data["aws_region"]["value"] == "asdz"
       assert data["aws_secret_access_key"]["value"] == "asdasdasdasdasd"
+      assert data["enable_standalone"]["value"] == false
     end
 
-    test "updates a list of settings with failures", meta do
+    test_with_auths "updates a list of settings with failures" do
       response =
-        provider_request("/configuration.update", %{
+        request("/configuration.update", %{
           base_url: "new_base_url.example",
           redirect_url_prefixes: ["new_base_url.example", "something.else"],
           fake_setting: "my_value",
           max_per_page: true,
           email_adapter: "fake",
+          enable_standalone: true,
           config_pid: meta[:config_pid]
         })
 
@@ -76,6 +87,9 @@ defmodule AdminAPI.V1.ProviderAuth.ConfigurationControllerTest do
 
       assert data["base_url"] != nil
       assert data["base_url"]["value"] == "new_base_url.example"
+
+      assert data["enable_standalone"] != nil
+      assert data["enable_standalone"]["value"] == true
 
       assert data["redirect_url_prefixes"] != nil
       assert data["redirect_url_prefixes"]["value"] == ["new_base_url.example", "something.else"]
@@ -103,9 +117,9 @@ defmodule AdminAPI.V1.ProviderAuth.ConfigurationControllerTest do
              }
     end
 
-    test "reloads app env", meta do
+    test_with_auths "reloads app env" do
       response =
-        provider_request("/configuration.update", %{
+        request("/configuration.update", %{
           base_url: "new_base_url.example",
           config_pid: meta[:config_pid]
         })
@@ -115,7 +129,72 @@ defmodule AdminAPI.V1.ProviderAuth.ConfigurationControllerTest do
       assert Application.get_env(:admin_api, :base_url, "new_base_url.example")
     end
 
-    test "generates an activity log", meta do
+    test_with_auths "returns unauthorized error if the admin is not from the master account" do
+      auth_token = insert(:auth_token, owner_app: "admin_api")
+      key = insert(:key)
+
+      opts = [
+        user_id: auth_token.user.id,
+        auth_token: auth_token.token,
+        access_key: key.access_key,
+        secret_key: key.secret_key
+      ]
+
+      response =
+        request(
+          "/configuration.update",
+          %{
+            base_url: "new_base_url.example",
+            config_pid: meta[:config_pid]
+          },
+          opts
+        )
+
+      assert response ==
+               %{
+                 "success" => false,
+                 "version" => "1",
+                 "data" => %{
+                   "object" => "error",
+                   "code" => "unauthorized",
+                   "description" => "You are not allowed to perform the requested operation.",
+                   "messages" => nil
+                 }
+               }
+    end
+
+    defp assert_update_logs(logs, originator, target) do
+      assert Enum.count(logs) == 1
+
+      logs
+      |> Enum.at(0)
+      |> assert_activity_log(
+        action: "update",
+        originator: originator,
+        target: %StoredSetting{uuid: target.uuid},
+        changes: %{"data" => %{"value" => target.value}, "position" => target.position},
+        encrypted_changes: %{}
+      )
+    end
+
+    test "generates an activity log for an admin request", meta do
+      timestamp = DateTime.utc_now()
+
+      response =
+        admin_user_request("/configuration.update", %{
+          base_url: "new_base_url.example",
+          config_pid: meta[:config_pid]
+        })
+
+      assert response["success"] == true
+      setting = Config.get_setting(:base_url)
+
+      timestamp
+      |> get_all_activity_logs_since()
+      |> assert_update_logs(get_test_admin(), setting)
+    end
+
+    test "generates an activity log for a provider request", meta do
       timestamp = DateTime.utc_now()
 
       response =
@@ -127,18 +206,9 @@ defmodule AdminAPI.V1.ProviderAuth.ConfigurationControllerTest do
       assert response["success"] == true
       setting = Config.get_setting(:base_url)
 
-      logs = get_all_activity_logs_since(timestamp)
-      assert Enum.count(logs) == 1
-
-      logs
-      |> Enum.at(0)
-      |> assert_activity_log(
-        action: "update",
-        originator: get_test_key(),
-        target: %StoredSetting{uuid: setting.uuid},
-        changes: %{"data" => %{"value" => "new_base_url.example"}, "position" => setting.position},
-        encrypted_changes: %{}
-      )
+      timestamp
+      |> get_all_activity_logs_since()
+      |> assert_update_logs(get_test_key(), setting)
     end
   end
 end
