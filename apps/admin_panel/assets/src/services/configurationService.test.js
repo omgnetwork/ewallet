76f2c592df904b22bb20b10caedc63a07dce86f3
jarindr <jarindr23@gmail.com>
2019-01-23 18:19:32 +0700

    fix omit key only on 0 value

--- apps/admin_panel/assets/src/services/configurationService.test.js
+++ apps/admin_panel/assets/src/services/configurationService.test.js
@@ -2,25 +2,30 @@ import { updateConfiguration } from './configurationService'
 import * as apiService from '../services/apiService'
 jest.mock('./apiService.js')
 describe('configurationService', () => {
-  test('it should omit null, undefined, Nan, 0, "" value out before sending', () => {})
-  apiService.authenticatedRequest.mockImplementation(() => {
-    return Promise.resolve({
-      data: {
-        success: true
-      }
+  test('it should omit null, undefined, Nan, 0, "" value out before sending', () => {
+    apiService.authenticatedRequest.mockImplementation(() => {
+      return Promise.resolve({
+        data: {
+          success: true
+        }
+      })
     })
-  })
-  updateConfiguration({
-    baseUrl: 'url',
-    redirectUrlPrefixes: [],
-    maxPerPage: null,
-    minPasswordLength: undefined,
-    senderEmail: NaN,
-    smtpUsername: ''
-  }).then(() => {
-    expect(apiService.authenticatedRequest).toBeCalledWith({
-      path: '/configuration.update',
-      data: { baseUrl: 'url' }
+    return updateConfiguration({
+      baseUrl: 'url',
+      redirectUrlPrefixes: [],
+      maxPerPage: null,
+      minPasswordLength: undefined,
+      senderEmail: NaN,
+      smtpUsername: ''
+    }).then(() => {
+      expect(apiService.authenticatedRequest).toBeCalledWith({
+        path: '/configuration.update',
+        data: {
+          base_url: 'url',
+          redirect_url_prefixes: [],
+          smtp_username: ''
+        }
+      })
     })
   })
 })
