84279378ea909776ae2d4a72f306e2b8c4de33c6
Unnawut Leepaisalsuwanna <921194+unnawut@users.noreply.github.com>
2019-01-28 12:45:23 +0700

    Bump up to v.1.1.0-pre.2 (#741)

--- apps/ewallet/mix.exs
+++ apps/ewallet/mix.exs
@@ -4,7 +4,7 @@ defmodule EWallet.Mixfile do
   def project do
     [
       app: :ewallet,
-      version: "1.1.0-pre.0",
+      version: "1.1.0-pre.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
