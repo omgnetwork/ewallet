4296f1a230b95db41b5fce5cf13ce7112c07bf0b
jarindr <jarindr23@gmail.com>
2019-01-31 14:33:40 +0700

    add default address for transaction request

--- apps/admin_panel/assets/src/omg-create-transaction-request-modal/index.js
+++ apps/admin_panel/assets/src/omg-create-transaction-request-modal/index.js
@@ -160,7 +160,7 @@ class CreateTransactionRequest extends Component {
         amount: formatAmount(this.state.amount, _.get(this.state.selectedToken, 'subunit_to_unit')),
         tokenId: _.get(this.state, 'selectedToken.id'),
         address: this.state.address || _.get(this.props, 'primaryWallet.address'),
-        accountId: _.get(this.state, 'selectedWallet.account_id'),
+        accountId: _.get(this.state, 'selectedWallet.account_id', this.props.match.params.accountId),
         expirationDate: this.state.expirationDate && moment(this.state.expirationDate).toISOString()
       })
       if (result.data) {
