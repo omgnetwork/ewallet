501176383816a88c23bc55196e0cdf9be1b37008
Unnawut Leepaisalsuwanna <unnawut@omise.co>
2019-02-04 20:23:00 +0700

    Revert "Update docker-gen.sh DEV_MODE to use dev-80264b0 builder image"
    This reverts commit 909b68420101826a4790d0e3f1dc267a6b643207.

--- docker-gen.sh
+++ docker-gen.sh
@@ -62,7 +62,7 @@ done
 
 if [ -z "$IMAGE_NAME" ]; then
    if [ $DEV_MODE = 1 ]; then
-       IMAGE_NAME="omisegoimages/ewallet-builder:dev-80264b0"
+       IMAGE_NAME="omisegoimages/ewallet-builder:stable"
    else
        IMAGE_NAME="omisego/ewallet:latest"
    fi
