d5a962be9da4d411caa9f77622e3cbfbf0aba12e
Unnawut Leepaisalsuwanna <unnawut@omise.co>
2019-02-04 18:38:50 +0700

    Increase :queue_target and :queue_interval on test environments to fix test timeouts

--- apps/ewallet_db/config/test.exs
+++ apps/ewallet_db/config/test.exs
@@ -4,4 +4,6 @@ config :ewallet_db, EWalletDB.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
   url: {:system, "DATABASE_URL", "postgres://localhost/ewallet_test"},
-  migration_timestamps: [type: :naive_datetime_usec]
+  migration_timestamps: [type: :naive_datetime_usec],
+  queue_target: 1_000,
+  queue_interval: 5_000
--- apps/local_ledger_db/config/test.exs
+++ apps/local_ledger_db/config/test.exs
@@ -4,4 +4,6 @@ config :local_ledger_db, LocalLedgerDB.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
   url: {:system, "LOCAL_LEDGER_DATABASE_URL", "postgres://localhost/local_ledger_test"},
-  migration_timestamps: [type: :naive_datetime_usec]
+  migration_timestamps: [type: :naive_datetime_usec],
+  queue_target: 1_000,
+  queue_interval: 5_000
