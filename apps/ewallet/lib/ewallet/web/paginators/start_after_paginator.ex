32c2164bb1fa62b72e33cd3c77e29e636300d6e7
euro <phuchit@omise.co>
2019-02-07 12:32:01 +0700

    :wrench: Prevent `created_at` column does not exist

--- apps/ewallet/lib/ewallet/web/paginators/start_after_paginator.ex
+++ apps/ewallet/lib/ewallet/web/paginators/start_after_paginator.ex
@@ -70,6 +70,10 @@ defmodule EWallet.Web.StartAfterPaginator do
     {:error, :invalid_parameter, "`start_by` must be a string"}
   end
 
+  def paginate_attrs(queryable, %{"sort_by" => "created_at"} = attrs, allowed_fields, repo) do
+    paginate_attrs(queryable, %{attrs | "sort_by" => "inserted_at"}, allowed_fields, repo)
+  end
+
   def paginate_attrs(
         queryable,
         %{"start_after" => nil, "start_by" => start_by} = attrs,
