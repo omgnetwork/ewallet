55fc4783873fefeaf61a0baa3b878181a6f854f2
James Kehoe <james.m.kehoe@gmail.com>
2019-02-17 07:51:46 -0500

    Change to Copyright 2018-2019

--- rel/commands/seed.sh
+++ rel/commands/seed.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright 2019 OmiseGO Pte Ltd
+# Copyright 2018-2019 OmiseGO Pte Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
