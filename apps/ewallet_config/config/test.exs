e088d55e7f053375c3db23fd803c11dc4c514d43
Unnawut Leepaisalsuwanna <unnawut@omise.co>
2019-02-18 17:20:27 +0700

    Increase :queue_target and :queue_interval for test environments

--- apps/ewallet_config/config/test.exs
+++ apps/ewallet_config/config/test.exs
@@ -5,5 +5,5 @@ config :ewallet_config, EWalletConfig.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   url: {:system, "DATABASE_URL", "postgres://localhost/ewallet_test"},
   migration_timestamps: [type: :naive_datetime_usec],
-  queue_target: 1_000,
-  queue_interval: 5_000
+  queue_target: 5_000,
+  queue_interval: 10_000
