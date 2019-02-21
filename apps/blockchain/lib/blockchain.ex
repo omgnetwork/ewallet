6dd69e04bbfe62d11218bc273011d6616390981a
Kridsada Thanabulpong <sirn@omise.co>
2019-02-21 23:25:29 +0700

    Add typedocs.

--- apps/blockchain/lib/blockchain.ex
+++ apps/blockchain/lib/blockchain.ex
@@ -3,8 +3,23 @@ defmodule Blockchain do
   Documentation for Blockchain.
   """
 
+  @typedoc """
+  A tuple of `{backend, wallet_id}` representing an identity of a backend.
+  The `wallet_id` may be `nil` to indicate a generic backend for non-wallet
+  specific operations.
+  """
   @type backend :: {atom(), String.t() | nil}
-  @type call :: {atom(), arity()} | atom()
 
+  @typedoc """
+  A tuple of `{func, args}` for passing to a remote functional call
+  to a backend. In case the remote function doesn't accept any arguments,
+  providing just `func` as an atom is suffice.
+  """
+  @type call :: {atom(), list()} | atom()
+
+  @typedoc """
+  A tuple of `{backend, wallet_id, public_key}` representing a wallet
+  currently managed by a backend.
+  """
   @type address :: {atom(), String.t(), String.t()}
 end
