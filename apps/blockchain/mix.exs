e9cdcd7f0a397e0d476324d2cc2f8f1df036e6ee
Kridsada Thanabulpong <sirn@omise.co>
2019-02-21 23:24:25 +0700

    Make sure version is consistent.

--- apps/blockchain/mix.exs
+++ apps/blockchain/mix.exs
@@ -4,12 +4,12 @@ defmodule Blockchain.MixProject do
   def project do
     [
       app: :blockchain,
-      version: "0.1.0",
+      version: "1.2.0-dev",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.6",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
