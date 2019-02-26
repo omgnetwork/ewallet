5b9f40d4576719dcc72d040377087d1164d9d225
Kridsada Thanabulpong <sirn@ogsite.net>
2019-02-26 12:35:37 +0900

    Initial blockchain integration (#807)

---
+++ apps/blockchain_eth/test/blockchain_eth/worker_test.exs
@@ -0,0 +1,35 @@
+# Copyright 2019 OmiseGO Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule BlockchainEth.WorkerTest do
+  use ExUnit.Case
+  alias BlockchainEth.Worker
+
+  setup do
+    {:ok, pid} = Worker.start_link()
+    %{pid: pid}
+  end
+
+  describe "generate_wallet/0" do
+    test "generates a ECDH keypair and wallet id", state do
+      {:ok, wallet_id, public_key} = Worker.generate_wallet(state[:pid])
+
+      assert is_binary(wallet_id)
+      assert byte_size(wallet_id) == 66
+
+      assert is_binary(public_key)
+      assert byte_size(public_key) == 130
+    end
+  end
+end
