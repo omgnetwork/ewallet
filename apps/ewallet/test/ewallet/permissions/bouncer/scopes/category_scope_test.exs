b32b3df88af8094aa2c01062daa9a2f8deee2bf3
Thibault Denizet <thibault@omise.co>
2019-02-28 11:38:08 +0700

    Category scope tests

--- apps/ewallet/test/ewallet/permissions/bouncer/scopes/category_scope_test.exs
+++ apps/ewallet/test/ewallet/permissions/bouncer/scopes/category_scope_test.exs
@@ -15,10 +15,131 @@
 defmodule EWallet.Bouncer.CategoryScopeTest do
   use EWallet.DBCase, async: true
   import EWalletDB.Factory
-  alias EWallet.Bouncer.KeyActor
-  alias EWalletDB.Membership
+  alias EWallet.Bouncer.{CategoryScope, Permission}
+  alias EWalletDB.{Repo, Category}
   alias ActivityLogger.System
 
-  describe "scope_query/1" do
+  describe "scope_query/1 with global abilities" do
+    test "returns Category as queryable when 'global' ability" do
+      actor = insert(:admin)
+
+      category_1 = insert(:category)
+      category_2 = insert(:category)
+      category_3 = insert(:category)
+
+      {:ok, _} = Category.delete(category_3, %System{})
+
+      permission = %Permission{
+        actor: actor,
+        global_abilities: %{categories: :global},
+        account_abilities: %{}
+      }
+
+      query = CategoryScope.scoped_query(permission)
+      category_uuids = query |> Repo.all() |> Enum.map(fn a -> a.uuid end)
+
+      assert length(category_uuids) == 2
+      assert Enum.member?(category_uuids, category_1.uuid)
+      assert Enum.member?(category_uuids, category_2.uuid)
+      refute Enum.member?(category_uuids, category_3.uuid)
+    end
+
+    test "returns all activity_logs the actor has access to when 'accounts' ability" do
+      actor = insert(:admin)
+
+      permission = %Permission{
+        actor: actor,
+        global_abilities: %{categories: :accounts},
+        account_abilities: %{}
+      }
+
+      assert CategoryScope.scoped_query(permission) == nil
+    end
+
+    test "returns Category as queryable when 'self' ability" do
+      actor = insert(:admin)
+
+      permission = %Permission{
+        actor: actor,
+        global_abilities: %{categories: :self},
+        account_abilities: %{}
+      }
+
+      assert CategoryScope.scoped_query(permission) == nil
+    end
+
+    test "returns nil as queryable when 'none' ability" do
+      actor = insert(:admin)
+
+      permission = %Permission{
+        actor: actor,
+        global_abilities: %{categories: :none},
+        account_abilities: %{}
+      }
+
+      assert CategoryScope.scoped_query(permission) == nil
+    end
+  end
+
+  describe "scope_query/1 with account abilities" do
+    test "returns Category as queryable when 'global' ability" do
+      actor = insert(:admin)
+
+      category_1 = insert(:category)
+      category_2 = insert(:category)
+      category_3 = insert(:category)
+
+      {:ok, _} = Category.delete(category_3, %System{})
+
+      permission = %Permission{
+        actor: actor,
+        global_abilities: %{},
+        account_abilities: %{categories: :global}
+      }
+
+      query = CategoryScope.scoped_query(permission)
+      category_uuids = query |> Repo.all() |> Enum.map(fn a -> a.uuid end)
+
+      assert length(category_uuids) == 2
+      assert Enum.member?(category_uuids, category_1.uuid)
+      assert Enum.member?(category_uuids, category_2.uuid)
+      refute Enum.member?(category_uuids, category_3.uuid)
+    end
+
+    test "returns all activity_logs the actor has access to when 'accounts' ability" do
+      actor = insert(:admin)
+
+      permission = %Permission{
+        actor: actor,
+        global_abilities: %{},
+        account_abilities: %{categories: :accounts}
+      }
+
+      assert CategoryScope.scoped_query(permission) == nil
+    end
+
+    test "returns Category as queryable when 'self' ability" do
+      actor = insert(:admin)
+
+      permission = %Permission{
+        actor: actor,
+        global_abilities: %{},
+        account_abilities: %{categories: :self}
+      }
+
+      assert CategoryScope.scoped_query(permission) == nil
+    end
+
+    test "returns nil as queryable when 'none' ability" do
+      actor = insert(:admin)
+
+      permission = %Permission{
+        actor: actor,
+        global_abilities: %{},
+        account_abilities: %{categories: :none}
+      }
+
+      assert CategoryScope.scoped_query(permission) == nil
+    end
   end
 end
