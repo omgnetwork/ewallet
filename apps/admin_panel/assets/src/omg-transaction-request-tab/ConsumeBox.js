5938a922f545f374c6ab4423199aa24872acbf0f
jarindr <jarindr23@gmail.com>
2019-03-04 15:45:03 +0700

    use all wallet fetcher for compatablity

--- apps/admin_panel/assets/src/omg-transaction-request-tab/ConsumeBox.js
+++ apps/admin_panel/assets/src/omg-transaction-request-tab/ConsumeBox.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import WalletsFetcher from '../omg-wallet/walletsFetcher'
+import WalletsFetcher from '../omg-wallet/allWalletsFetcher'
 import TokensFetcher from '../omg-token/tokensFetcher'
 import QR from './QrCode'
 import { formatAmount, formatReceiveAmountToTotal } from '../utils/formatter'
