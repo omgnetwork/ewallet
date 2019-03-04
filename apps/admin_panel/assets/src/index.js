987fddbc6fdf6d164c69b270ee251d78d86e85d4
jarindr <jarindr23@gmail.com>
2019-03-04 15:26:16 +0700

    setup default format moment

--- apps/admin_panel/assets/src/index.js
+++ apps/admin_panel/assets/src/index.js
@@ -2,6 +2,10 @@ import { render } from 'react-dom'
 import App from './app'
 import React from 'react'
 import { store } from './store'
+import moment from 'moment'
+
+// SETUP DEFAULT FORMAT FOR MOMENT
+moment.defaultFormat = 'ddd, DD/MM/YYYY HH:mm:ss'
 
 // RENDER APP
 render(<App store={store} />, document.getElementById('app'))
