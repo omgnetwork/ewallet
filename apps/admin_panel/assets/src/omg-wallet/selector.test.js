d0175a524ef46fe41dbfdd835b396d271e0a8a1d
jarindr <jarindr23@gmail.com>
2019-03-06 11:40:59 +0700

    fix tests

--- apps/admin_panel/assets/src/omg-wallet/selector.test.js
+++ apps/admin_panel/assets/src/omg-wallet/selector.test.js
@@ -3,7 +3,7 @@ import { selectPrimaryWalletByAccountId } from './selector'
 describe('selectors wallet', () => {
   test('should select primary wallet of current account', () => {
     const state = {
-      accounts: { id: 'a' },
+      accounts: { 'a': { id: 'a' } },
       wallets: {
         b: {
           account_id: 'a',
@@ -15,7 +15,9 @@ describe('selectors wallet', () => {
         }
       }
     }
-    expect(selectPrimaryWalletByAccountId(state)('a')).toEqual({
+
+    const primaryWallet = selectPrimaryWalletByAccountId(state)('a')
+    expect(primaryWallet).toEqual({
       account_id: 'a',
       identifier: 'primary'
     })
