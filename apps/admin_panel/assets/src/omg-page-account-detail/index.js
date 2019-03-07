0c3d9625c4e6eb19ca33849f9a60496c8be57951
jarindr <jarindr23@gmail.com>
2019-03-08 00:06:52 +0700

    update account detail flex

--- apps/admin_panel/assets/src/omg-page-account-detail/index.js
+++ apps/admin_panel/assets/src/omg-page-account-detail/index.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled, { withTheme } from 'styled-components'
-import { withRouter, Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import AccountProvider from '../omg-account/accountProvider'
-import WalletsFetcherByAccountId from '../omg-wallet/walletsFetcher'
 import { compose } from 'recompose'
-import { formatReceiveAmountToTotal } from '../utils/formatter'
 import Section, { DetailGroup } from '../omg-page-detail-layout/DetailSection'
 import TopBar from '../omg-page-detail-layout/TopBarDetail'
-import DetailLayout from '../omg-page-detail-layout/DetailLayout'
 import moment from 'moment'
 import Copy from '../omg-copy'
 const AccountDetailContainer = styled.div``
@@ -16,20 +13,12 @@ const ContentDetailContainer = styled.div`
   margin-top: 40px;
   display: flex;
   > div {
-    flex: 0 1 50%;
+    flex: 1 1 50%;
   }
 `
 const ContentContainer = styled.div`
   display: inline-block;
   width: 100%;
-`
-const WalletCointainter = styled.div`
-  b {
-    text-transform: capitalize;
-  }
-`
-const TokensContainer = styled.div`
-  margin-bottom: 40px;
 `
 const enhance = compose(
   withTheme,
@@ -62,48 +51,6 @@ class AccountDetailPage extends Component {
         <DetailGroup>
           <b>Last Update:</b> {moment(account.updated_at).format()}
         </DetailGroup>
-      </Section>
-    )
-  }
-  renderWallets = () => {
-    const accountId = this.props.match.params.accountId
-    return (
-      <Section title='WALLETS'>
-        <WalletsFetcherByAccountId
-          query={{ accountId }}
-          render={({ data }) => {
-            return data.map(wallet => {
-              return (
-                <WalletCointainter key={wallet.address}>
-                  <DetailGroup>
-                    <b>{wallet.name} Wallet</b>
-                    <Link to={`/${accountId}/wallets/${wallet.address}`}>{wallet.address}</Link>
-                  </DetailGroup>
-                  {!!wallet.balances.filter(b => b.amount).length && (
-                    <TokensContainer>
-                      {wallet.balances
-                        .filter(b => b.amount)
-                        .map(balance => {
-                          return (
-                            <DetailGroup key={balance.token.id}>
-                              <b>{balance.token.name}</b>
-                              <span>
-                                {formatReceiveAmountToTotal(
-                                  balance.amount,
-                                  balance.token.subunit_to_unit
-                                )}
-                              </span>{' '}
-                              <span>{balance.token.symbol}</span>
-                            </DetailGroup>
-                          )
-                        })}
-                    </TokensContainer>
-                  )}
-                </WalletCointainter>
-              )
-            })
-          }}
-        />
       </Section>
     )
   }
