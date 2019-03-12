456cfd0ae600467f3c4f322fbf56215f84d3a175
Thibault Denizet <thibault@omise.co>
2019-03-12 14:23:16 +0700

    Attempt to configure queue interval for ewallet app

--- apps/ewallet/config/test.exs
+++ apps/ewallet/config/test.exs
@@ -1,3 +1,6 @@
 use Mix.Config
 
-config :ewallet, websocket_endpoints: [EWallet.TestEndpoint]
+config :ewallet,
+  websocket_endpoints: [EWallet.TestEndpoint],
+  queue_target: 1_000,
+  queue_interval: 5_000
