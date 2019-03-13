019666a6f837a861824b84763b7882dce750b95f
Unnawut Leepaisalsuwanna <unnawut@omise.co>
2019-03-13 22:42:16 +0700

    Fix syntax error

--- apps/ewallet_db/config/test.exs
+++ apps/ewallet_db/config/test.exs
@@ -2,6 +2,6 @@ use Mix.Config
 
 config :ewallet_db, EWalletDB.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
-  url: {:system, "DATABASE_URL", "postgres://localhost/ewallet_test"}
+  url: {:system, "DATABASE_URL", "postgres://localhost/ewallet_test"},
   queue_target: 1_500,
   queue_interval: 5_000
