dbb1d696118b4be887c49f5eb79b11d961661950
Unnawut Leepaisalsuwanna <unnawut@omise.co>
2019-03-13 22:40:13 +0700

    Configure DB.SharedConnectionPool for ActivityLogger

--- apps/activity_logger/config/config.exs
+++ apps/activity_logger/config/config.exs
@@ -7,4 +7,10 @@ config :activity_logger,
   schemas_to_activity_log_config: %{ActivityLogger.System => %{type: "system", identifier: nil}},
   activity_log_types_to_schemas: %{"system" => ActivityLogger.System}
 
+config :activity_logger, ActivityLogger.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  pool: DB.SharedConnectionPool,
+  shared_pool_id: :ewallet,
+  migration_timestamps: [type: :naive_datetime_usec]
+
 import_config "#{Mix.env()}.exs"
