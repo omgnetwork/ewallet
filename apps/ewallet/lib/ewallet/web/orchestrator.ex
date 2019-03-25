10d11030bb25cdcdbe38c129dfea639569988ac9
Unnawut Leepaisalsuwanna <unnawut@omise.co>
2019-03-25 16:10:13 +0700

    Remove unused lines

--- apps/ewallet/lib/ewallet/web/orchestrator.ex
+++ apps/ewallet/lib/ewallet/web/orchestrator.ex
@@ -48,9 +48,6 @@ defmodule EWallet.Web.Orchestrator do
       {:error, :not_allowed, field} ->
         {:error, :query_field_not_allowed, field_name: field}
 
-      # {:error, :missing_filter_param, params} ->
-      #   {:error, :missing_filter_param, filter_params: params}
-
       {:error, _, _} = error ->
         error
     end
