f8e5dbe8cf22e194882f12bf2dea1f1c0db11927
euro <phuchit@omise.co>
2019-04-03 12:18:29 +0700

    :sparkles: Implement controller for 2FA apis

---
+++ apps/admin_api/lib/admin_api/v1/controllers/two_factor_auth_controller.ex
@@ -0,0 +1,100 @@
+# Copyright 2018-2019 OmiseGO Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule AdminAPI.V1.TwoFactorAuthController do
+  use AdminAPI, :controller
+  import AdminAPI.V1.ErrorHandler
+  alias AdminAPI.V1.AdminAPIAuth
+  alias EWallet.{TwoFactorPolicy, TwoFactorAuthenticator}
+  alias EWallet.Web.{Orchestrator, V1.AuthTokenOverlay}
+  alias EWalletDB.{AuthToken, User}
+  alias Ecto.Changeset
+
+  def verify(conn, attrs) do
+    with auth_params <- verify_user_authenticated(conn),
+         {:ok, user} <- verify_user_2fa_enabled(auth_params),
+         {:ok, _} <- authorize(:verify, auth_params, user),
+         {:ok} <- TwoFactorAuthenticator.verify(attrs, user),
+         {:ok, token} = AuthToken.generate(user, :admin_api, user, true),
+         {:ok, token} <- Orchestrator.one(token, AuthTokenOverlay, attrs) do
+      render(conn, :auth_token, %{auth_token: token})
+    else
+      error -> respond_error(error, conn)
+    end
+  end
+
+  defp verify_user_authenticated(conn) do
+    AdminAPIAuth.authenticate(%{headers: conn.req_headers})
+  end
+
+  defp verify_user_2fa_enabled(%{admin_user: admin_user} = attrs) do
+    IO.inspect(attrs)
+    do_verify_user_2fa_enabled(admin_user)
+  end
+
+  defp verify_user_2fa_enabled(%{auth_error: error_code}) when is_atom(error_code) do
+    {:error, error_code}
+  end
+
+  defp do_verify_user_2fa_enabled(%{enabled_2fa_at: nil}), do: {:error, :invalid_parameter}
+
+  defp do_verify_user_2fa_enabled(%{enabled_2fa_at: _} = user), do: {:ok, user}
+
+  def create_backup_codes(conn, _), do: do_create(conn, :backup_codes)
+
+  def create_secret_code(conn, _), do: do_create(conn, :secret_code)
+
+  defp do_create(conn, type) do
+    with %User{} = user <- conn.assigns[:admin_user] || {:error, :unauthorized},
+         {:ok, _} <- authorize(:create, conn.assigns, user),
+         {:ok, result} <- TwoFactorAuthenticator.create_and_update(user, type) do
+      render(conn, :create, result)
+    else
+      error -> respond_error(error, conn)
+    end
+  end
+
+  def enable(conn, attrs), do: do_enable(conn, attrs, true)
+
+  def disable(conn, attrs), do: do_enable(conn, attrs, false)
+
+  defp do_enable(conn, attrs, enable) do
+    with %User{} = user <- conn.assigns[:admin_user] || {:error, :unauthorized},
+         auth_token_string <- conn.private[:auth_auth_token],
+         {:ok, _} <- authorize(:create, conn.assigns, user),
+         {:ok} <- TwoFactorAuthenticator.verify(attrs, user),
+         {:ok, token} <- TwoFactorAuthenticator.enable(user, auth_token_string, :admin_api, enable),
+         {:ok, token} <- Orchestrator.one(token, AuthTokenOverlay, attrs) do
+      render(conn, :auth_token, %{auth_token: token})
+    else
+      error -> respond_error(error, conn)
+    end
+  end
+
+  defp respond_error({:error, error}, conn) when is_atom(error) do
+    handle_error(conn, error)
+  end
+
+  defp respond_error({:error, code, description}, conn) do
+    handle_error(conn, code, description)
+  end
+
+  defp respond_error({:error, %Changeset{} = changeset}, conn) do
+    handle_error(conn, :invalid_parameter, changeset)
+  end
+
+  defp authorize(action, actor, target) do
+    TwoFactorPolicy.authorize(action, actor, target)
+  end
+end
--- apps/admin_api/lib/admin_api/v1/router.ex
+++ apps/admin_api/lib/admin_api/v1/router.ex
@@ -227,6 +227,12 @@ defmodule AdminAPI.V1.Router do
     # Activity logs endpoint
     post("/activity_log.all", ActivityLogController, :all)
 
+    # 2FA endpoints
+    post("/2fa.create_backup_codes", TwoFactorAuthController, :create_backup_codes)
+    post("/2fa.create_secret_code", TwoFactorAuthController, :create_secret_code)
+    post("/2fa.enable", TwoFactorAuthController, :enable)
+    post("/2fa.disable", TwoFactorAuthController, :disable)
+
     # Self endpoints (operations on the currently authenticated user)
     post("/me.get", SelfController, :get)
     post("/me.get_accounts", SelfController, :get_accounts)
@@ -244,12 +250,14 @@ defmodule AdminAPI.V1.Router do
   end
 
   # Public endpoints and Fallback endpoints.
-  # Gandles all remaining routes
+  # Handles all remaining routes
   # that are not handled by the scopes above.
   scope "/", AdminAPI.V1 do
     pipe_through([:api])
 
     post("/admin.login", AdminAuthController, :login)
+    post("/2fa.verify_passcode", TwoFactorAuthController, :verify)
+    post("/2fa.verify_backup_code", TwoFactorAuthController, :verify)
     post("/invite.accept", InviteController, :accept)
 
     # Forget Password endpoints
