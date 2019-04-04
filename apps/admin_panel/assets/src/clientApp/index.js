8381c980e358102388b270ebe8fe60f223b99881
jarindr <jarindr23@gmail.com>
2019-04-04 14:43:35 +0700

    add page

--- apps/admin_panel/assets/src/clientApp/index.js
+++ apps/admin_panel/assets/src/clientApp/index.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader/root'
 import 'reset-css'
+import { ThemeProvider } from 'styled-components'
+import theme from '../adminPanelApp/theme'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import ResetPasswordForm from '../omg-forgetPassword-form'
 class App extends Component {
   componentDidCatch () {
     return 'Something very bad happened, please contact admin.'
   }
   render () {
     return (
-      <div>ClientApp</div>
+      <ThemeProvider theme={theme}>
+        <Router basename='/client'>
+          <Switch>
+            <Route path='/reset-password' exact component={ResetPasswordForm} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
     )
   }
 }
