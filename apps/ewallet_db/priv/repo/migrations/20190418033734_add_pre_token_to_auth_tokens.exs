371380b7f698d373c58786ffc9607efacdd7b343
euro <phuchit@omise.co>
2019-04-23 18:20:30 +0700

    :card_file_box: Add create `pre_auth_token` script

--- apps/ewallet_db/priv/repo/migrations/20190418033734_add_pre_token_to_auth_tokens.exs
+++
@@ -1,9 +0,0 @@
-defmodule EWalletDB.Repo.Migrations.AddPreTokenToAuthTokens do
-  use Ecto.Migration
-
-  def change do
-    alter table(:auth_token) do
-      add :pre_token, :string
-    end
-  end
-end
