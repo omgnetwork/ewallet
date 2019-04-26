16bd53ac56a8443dcb7482e91bd86188a69dcbab
Kridsada Thanabulpong <sirn@omise.co>
2019-04-26 23:28:57 +0700

    Configure keychain secret key in docker-gen.sh

--- docker-gen.sh
+++ docker-gen.sh
@@ -40,6 +40,7 @@ POSTGRES_PASSWORD=""
 EXTERNAL_NETWORK=""
 EWALLET_SECRET_KEY=""
 LOCAL_LEDGER_SECRET_KEY=""
+KEYCHAIN_SECRET_KEY=""
 DEV_MODE=0
 
 while true; do
@@ -49,6 +50,7 @@ while true; do
         -p ) POSTGRES_PASSWORD=$2;       shift; shift;;
         -k ) EWALLET_SECRET_KEY=$2;      shift; shift;;
         -K ) LOCAL_LEDGER_SECRET_KEY=$2; shift; shift;;
+        -J ) KEYCHAIN_SECRET_KEY=$2;     shift; shift;;
         -f ) ENV_FILE=$2;                shift; shift;;
         -d ) DEV_MODE=1;  shift;;
         -h ) print_usage; exit 2;;
@@ -58,6 +60,7 @@ done
 
 [ -z "$EWALLET_SECRET_KEY" ]      && EWALLET_SECRET_KEY=$(openssl rand -base64 32)
 [ -z "$LOCAL_LEDGER_SECRET_KEY" ] && LOCAL_LEDGER_SECRET_KEY=$(openssl rand -base64 32)
+[ -z "$KEYCHAIN_SECRET_KEY" ]     && KEYCHAIN_SECRET_KEY=$(openssl rand -base64 32)
 [ -z "$POSTGRES_PASSWORD" ]       && POSTGRES_PASSWORD=$(openssl rand -base64 24 | tr '+/' '-_')
 
 if [ -z "$IMAGE_NAME" ]; then
@@ -81,7 +84,8 @@ YML_SERVICES="
       DATABASE_URL: postgresql://postgres:$POSTGRES_PASSWORD@postgres:5432/ewallet
       LOCAL_LEDGER_DATABASE_URL: postgresql://postgres:$POSTGRES_PASSWORD@postgres:5432/local_ledger
       EWALLET_SECRET_KEY: $EWALLET_SECRET_KEY
-      LOCAL_LEDGER_SECRET_KEY: $LOCAL_LEDGER_SECRET_KEY\
+      LOCAL_LEDGER_SECRET_KEY: $LOCAL_LEDGER_SECRET_KEY
+      KEYCHAIN_SECRET_KEY: $KEYCHAIN_SECRET_KEY\
 " # EOF
 
 if [ -n "$ENV_FILE" ]; then
