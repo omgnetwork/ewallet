bc47a4de9a0feab68bef4d51cb5db33413e11590
nicholasmueller <nicholasmueller91@gmail.com>
2019-05-03 16:45:21 +0700

    api detail table pages

--- apps/admin_panel/assets/src/omg-page-api-detail-accounts/index.js
+++ apps/admin_panel/assets/src/omg-page-api-detail-accounts/index.js
@@ -1,11 +1,53 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-const KeyDetailAccountsPage = () => {
+import { Breadcrumb, Icon } from '../omg-uikit'
+import TopNavigation from '../omg-page-layout/TopNavigation'
+
+const BreadContainer = styled.div`
+  margin-top: 30px;
+  color: ${props => props.theme.colors.B100};
+  font-size: 14px;
+`
+const TitleContainer = styled.div`
+  span {
+    margin-left: 10px;
+  }
+`
+
+const KeyDetailAccountsPage = ({ match: { params } }) => {
+  const { keyType, keyDetail } = params
+
   return (
-    <div>
-      TOTO
-    </div>
+    <>
+      <BreadContainer>
+        <Breadcrumb
+          items={[
+            <Link key='keys' to={`/keys/${keyType}`}>Keys</Link>,
+            <Link key='key-detail' to={`/keys/${keyType}/${keyDetail}`}>{keyDetail}</Link>,
+            <span key='assigned-accounts'>Assigned Accounts</span>
+          ]}
+        />
+      </BreadContainer>
+
+      <TopNavigation
+        title={
+          <TitleContainer>
+            <Icon name='Key' />
+            <span>Assigned Accounts</span>
+          </TitleContainer>
+        }
+        searchBar={false}
+        divider={false}
+      />
+    </>
   )
+}
+
+KeyDetailAccountsPage.propTypes = {
+  match: PropTypes.object
 }
 
 export default KeyDetailAccountsPage
