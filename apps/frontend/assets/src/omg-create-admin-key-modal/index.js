f1e46394593aca401f7b544558d0178985886678
nicholasmueller <nicholasmueller91@gmail.com>
2019-05-21 10:42:33 +0700

    linter

--- apps/frontend/assets/src/omg-create-admin-key-modal/index.js
+++ apps/frontend/assets/src/omg-create-admin-key-modal/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
 import { compose } from 'recompose'
