b29f7812a59883f79ad68b7729e38e9a6531fc95
mederic <mederic@omise.co>
2019-06-13 18:13:17 +0700

    Increase gas limit to prevent failed transaction

--- apps/eth_blockchain/config/config.exs
+++ apps/eth_blockchain/config/config.exs
@@ -10,7 +10,7 @@ config :eth_blockchain,
 config :eth_blockchain,
   default_gas_price: 20_000_000_000,
   default_eth_transaction_gas_limit: 21_000,
-  default_contract_transaction_gas_limit: 42_000,
+  default_contract_transaction_gas_limit: 90_000,
   chain_id: 1
 
 # Import environment specific config. This must remain at the bottom
