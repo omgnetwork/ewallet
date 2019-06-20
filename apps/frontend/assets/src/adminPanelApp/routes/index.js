03aaf27a946b10259b90e5d1da2d69e8e31d1f5e
jarindr <jarindr23@gmail.com>
2019-06-21 01:16:49 +0700

    add history package

--- apps/frontend/assets/src/adminPanelApp/routes/index.js
+++ apps/frontend/assets/src/adminPanelApp/routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { BrowserRouter as Router, Redirect, Switch, Route } from 'react-router-dom'
-
+import { Router, Redirect, Switch, Route } from 'react-router-dom'
+import history from './history'
 import AuthenticatedRoute from './authenticatedRoute'
 import LoginRoute from './loginRoute'
 import LoginForm from '../../omg-login-form'
@@ -31,7 +31,7 @@ import ModalController from '../../omg-modal/ModalController'
 // prettier-ignore
 
 const createRoute = () => (
-  <Router basename='/admin'>
+  <Router basename='/admin' history={history}>
     <Switch>
       <Redirect from='/' to={'/accounts'} exact />
       <Redirect from='/accounts/:accountId' to='/accounts/:accountId/details' exact />
