136d77a8dfccaa3be493cd9a6542a59878206dd2
Mederic <mederic@omise.co>
2019-07-23 11:37:02 +0700

    Move validation to validate_blockchain

--- apps/ewallet_db/lib/ewallet_db/blockchain_wallet.ex
+++ apps/ewallet_db/lib/ewallet_db/blockchain_wallet.ex
@@ -61,12 +61,8 @@ defmodule EWalletDB.BlockchainWallet do
       ],
       required: [:address, :name, :type, :blockchain_identifier]
     )
-    |> unique_constraint(:address, name: :blockchain_wallet_blockchain_identifier_address_index)
-    |> unique_constraint(:public_key,
-      name: :blockchain_wallet_blockchain_identifier_public_key_index
-    )
+
     |> unique_constraint(:name)
-    |> validate_immutable(:address)
     |> validate_blockchain()
     |> validate_inclusion(:type, @wallet_types)
     |> validate_length(:address, count: :bytes, max: 255)
@@ -84,7 +80,6 @@ defmodule EWalletDB.BlockchainWallet do
       required: [:public_key]
     )
     |> merge(shared)
-    |> validate_immutable(:public_key)
     |> validate_inclusion(:type, [@hot])
     |> validate_length(:public_key, count: :bytes, max: 255)
   end
@@ -99,6 +94,13 @@ defmodule EWalletDB.BlockchainWallet do
     changeset
     |> validate_blockchain_address(:address)
     |> validate_blockchain_identifier(:blockchain_identifer)
+    |> unique_constraint(:address, name: :blockchain_wallet_blockchain_identifier_address_index)
+    |> unique_constraint(:public_key,
+      name: :blockchain_wallet_blockchain_identifier_public_key_index
+    )
+    |> validate_immutable(:address)
+    |> validate_immutable(:public_key)
+    |> validate_immutable(:blockchain_identifer)
   end
 
   def get_primary_hot_wallet(identifier) do
