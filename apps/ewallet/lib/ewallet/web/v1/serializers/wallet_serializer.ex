ae4249ae271289b3c91168dc4572dd27864d0cc9
Unnawut Leepaisalsuwanna <unnawut@omise.co>
2019-08-15 15:06:31 +0700

    Fix incorrect :blockchain_deposit_wallet retrieval

--- apps/ewallet/lib/ewallet/web/v1/serializers/wallet_serializer.ex
+++ apps/ewallet/lib/ewallet/web/v1/serializers/wallet_serializer.ex
@@ -58,7 +58,7 @@ defmodule EWallet.Web.V1.WalletSerializer do
       account: AccountSerializer.serialize(wallet.account),
       balances: serialize_balances(wallet),
       enabled: wallet.enabled,
-      blockchain_deposit_address: Assoc.get(wallet, [:deposit_wallet, :address]),
+      blockchain_deposit_address: Assoc.get(wallet, [:blockchain_deposit_wallet, :address]),
       created_at: DateFormatter.to_iso8601(wallet.inserted_at),
       updated_at: DateFormatter.to_iso8601(wallet.updated_at)
     }
