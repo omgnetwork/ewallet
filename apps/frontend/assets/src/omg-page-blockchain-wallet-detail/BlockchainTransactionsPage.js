3acb2ffe56190b47bd606394dd102568d353a327
nicholasmueller <nicholasmueller91@gmail.com>
2019-08-26 18:52:17 +0700

    implement blockchain transaction page

--- apps/frontend/assets/src/omg-page-blockchain-wallet-detail/BlockchainTransactionsPage.js
+++ apps/frontend/assets/src/omg-page-blockchain-wallet-detail/BlockchainTransactionsPage.js
@@ -1,7 +1,15 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/display-name */
 import React from 'react'
+import { withRouter } from 'react-router'
+import queryString from 'query-string'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
 import TabMenu from './TabMenu'
+
+import TransactionsFetcher from '../omg-transaction/transactionsFetcher'
+import TransactionTable from '../omg-page-transaction/TransactionTable'
 
 const TopRow = styled.div`
   display: flex;
@@ -10,15 +18,46 @@ const TopRow = styled.div`
   margin-bottom: 10px;
 `
 
-const BlockchainTransactionsPage = () => {
+const renderBlockchainTransactionPage = address => ({
+  data,
+  individualLoadingStatus,
+  pagination
+}) => {
+  const transactions = _.filter(data, i => {
+    return i.from_blockchain_address === address || i.to_blockchain_address === address
+  })
+
+  return (
+    <TransactionTable
+      loadingStatus={individualLoadingStatus}
+      pagination={pagination}
+      transactions={transactions}
+    />
+  )
+}
+
+const BlockchainTransactionsPage = ({ location, match }) => {
+  const { address } = match.params
   return (
     <>
       <TopRow>
         <TabMenu />
       </TopRow>
-      BlockchainTransactionsPage
+      <TransactionsFetcher
+        render={renderBlockchainTransactionPage(address)}
+        query={{
+          page: queryString.parse(location.search).page,
+          perPage: Math.floor(window.innerHeight / 100),
+          search: queryString.parse(location.search).search
+        }}
+      />
     </>
   )
 }
 
-export default BlockchainTransactionsPage
+BlockchainTransactionsPage.propTypes = {
+  location: PropTypes.object,
+  match: PropTypes.object
+}
+
+export default withRouter(BlockchainTransactionsPage)
