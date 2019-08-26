086547e1bacb2e92ccf50e47ad0743a5071f9fe8
Unnawut Leepaisalsuwanna <unnawut@omise.co>
2019-08-26 12:30:33 +0700

    Add missing EthBlockchain.Transaction tests

--- apps/eth_blockchain/test/eth_blockchain/transaction_test.exs
+++ apps/eth_blockchain/test/eth_blockchain/transaction_test.exs
@@ -16,6 +16,7 @@ defmodule EthBlockchain.TransactionTest do
   use EthBlockchain.EthBlockchainCase, async: true
 
   alias EthBlockchain.{Transaction, ABIEncoder}
+  alias ExthCrypto.Math
   alias Keychain.{Signature, Wallet}
   alias Utils.Helpers.Encoding
 
@@ -191,14 +192,103 @@ defmodule EthBlockchain.TransactionTest do
   end
 
   describe "transaction_hash/2" do
-    test "returns the hash of the given transaction"
+    test "returns the hash of the given transaction", state do
+      {:ok, encoded_trx} =
+        Transaction.send(
+          %{from: state[:valid_sender], to: state[:addr_1], amount: 100},
+          :dumb,
+          state[:pid]
+        )
+
+      trx = decode_transaction_response(encoded_trx)
+      hash_hex = Transaction.transaction_hash(trx, 10) |> Math.bin_to_hex()
+
+      assert hash_hex == "94949cf3c67b0cc3c203ff4b98734f8d466da74d034e2d030dba476622f0f9c1"
+    end
   end
 
   describe "serialize/2" do
-    test "returns the RLP-encoded transaction"
+    test "returns the RLP-encoded transaction including vrs", state do
+      {:ok, encoded_trx} =
+        Transaction.send(
+          %{from: state[:valid_sender], to: state[:addr_1], amount: 100},
+          :dumb,
+          state[:pid]
+        )
+
+      trx = decode_transaction_response(encoded_trx)
+      serialized =  Transaction.serialize(trx, true)
+
+      assert [
+        "",
+        <<4, 168, 23, 200, 0>>,
+        "R\b",
+        <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1>>,
+        "d",
+        "",
+        _,
+        _,
+        _
+      ] = serialized
+
+      # vrs change for every call, so assert only their size
+      assert serialized |> Enum.at(6) |> byte_size() == 3
+      assert serialized |> Enum.at(7) |> byte_size() == 32
+      assert serialized |> Enum.at(8) |> byte_size() == 32
+    end
+
+    test "returns the RLP-encoded transaction excluding vrs", state do
+      {:ok, encoded_trx} =
+        Transaction.send(
+          %{from: state[:valid_sender], to: state[:addr_1], amount: 100},
+          :dumb,
+          state[:pid]
+        )
+
+      trx = decode_transaction_response(encoded_trx)
+
+      assert Transaction.serialize(trx, false) == [
+        "",
+        <<4, 168, 23, 200, 0>>,
+        "R\b",
+        <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1>>,
+        "d",
+        ""
+      ]
+    end
   end
 
-  describe "deserialize/2" do
-    test "returns the decoded RLP-encoded transaction"
+  describe "deserialize/1" do
+    test "returns the decoded RLP-encoded transaction" do
+      deserialized =
+        Transaction.deserialize([
+          "",
+          <<4, 168, 23, 200, 0>>,
+          "R\b",
+          <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1>>,
+          "d",
+          "",
+          <<2, 193, 206>>,
+          <<132, 50, 201, 31, 172, 179, 191, 32, 115, 195, 31, 236, 245, 18,
+            240, 161, 185, 98, 151, 176, 179, 206, 94, 110, 26, 144, 24,
+            145, 38, 139, 9, 133>>,
+          <<68, 44, 170, 60, 51, 238, 50, 26, 71, 237, 212, 7, 160, 164,
+            189, 233, 114, 84, 184, 57, 240, 120, 183, 40, 193, 69, 42, 112,
+            253, 163, 237, 138>>
+        ])
+
+      assert deserialized == %Transaction{
+        data: "",
+        gas_limit: 21000,
+        gas_price: 20000000000,
+        init: "",
+        nonce: 0,
+        r: 59795026471191791691553212152080426284599024569572292875880667924209261545861,
+        s: 30836189894457032652897540276539634084883692778205447233810671240962434657674,
+        to: <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1>>,
+        v: 180686,
+        value: 100
+      }
+    end
   end
 end
