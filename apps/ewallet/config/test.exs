df51605d8ea1a9daff771299cf5cf0e3bfa7768f
Mederic <mederic@omise.co>
2019-09-05 11:39:10 +0700

    Remove TODO

--- apps/ewallet/config/test.exs
+++ apps/ewallet/config/test.exs
@@ -3,5 +3,4 @@ use Mix.Config
 config :ewallet,
   websocket_endpoints: [EWallet.TestEndpoint],
   eth_node_adapter: {:dumb, EthGethAdapter.DumbAdapter},
-  # TODO: update this for tests
   cc_node_adapter: {:dumb_cc, EthGethAdapter.DumbCCAdapter}
