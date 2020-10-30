f5f105e422fb1f21a3e8265fa9289d92648e858a
okalouti <omar@omisego.co>
2020-10-30 10:59:03 +0700

    feat: hide internal transfer button on blockchain enabled

---
+++ apps/frontend/assets/src/omg-transaction/CreateTransactionButton.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { Button, Icon } from 'omg-uikit'
+import { openModal } from 'omg-modal/action'
+import { selectBlockchainEnabled } from 'omg-configuration/selector'
+
+interface CreateTransactionButtonProps {
+  fromAddress?:string
+}
+
+function CreateTransactionButton ({ fromAddress }: CreateTransactionButtonProps) {
+  const blockchainEnabled = useSelector(selectBlockchainEnabled())
+  const dispatch = useDispatch()
+  const handleClick = () => {
+    dispatch(openModal({ id: 'createTransaction', fromAddress: fromAddress }))
+  }
+
+  return blockchainEnabled? null: (
+    <Button
+      key='create'
+      size='small'
+      styleType='primary'
+      onClick={handleClick}
+    >
+      <Icon name='Transaction' />
+      <span>Internal Transfer</span>
+    </Button>
+  )
+}
+
+export default CreateTransactionButton
