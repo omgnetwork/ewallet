b578df75cdf2ada901af3aa74b9ffa06e93794fb
okalouti <omar@omisego.co>
2020-10-31 13:53:01 +0700

    feat: redirect internal routes to 404 if blockchain enabled

--- apps/frontend/assets/src/adminPanelApp/routes/index.js
+++ apps/frontend/assets/src/adminPanelApp/routes/index.js
@@ -1,41 +1,45 @@
 import React from 'react'
 import { Router, Redirect, Switch, Route } from 'react-router-dom'
-import history from './history'
-import AuthenticatedRoute from './authenticatedRoute'
-import LoginRoute from './loginRoute'
-import LoginForm from '../../omg-login-form'
-import ForgetPasswordForm from '../../omg-forgetPassword-form'
-import CreateNewPasswordForm from '../../omg-createNewPassword-form'
-import InviteForm from '../../omg-invite-form'
-import AccountPage from '../../omg-page-account'
-import TokenPage from '../../omg-page-token'
-import TransactionPage from '../../omg-page-transaction'
-import WalletPage from '../../omg-page-wallets'
-import BlockchainWalletDetailPage from '../../omg-page-blockchain-wallet-detail'
-import BlockchainWalletsPage from '../../omg-page-blockchain-wallets'
-import UserSettingPage from '../../omg-page-user-setting'
-import ApiKeyPage from '../../omg-page-api'
-import ApiKeyDetailPage from '../../omg-page-api-detail'
-import KeyDetailAccountsPage from '../../omg-page-api-detail-accounts'
-import UserPage from '../../omg-page-users'
-import TokenDetailPage from '../../omg-page-token-detail'
-import WalletDetailPage from '../../omg-page-wallet-detail'
-import UserDetailPage from '../../omg-page-user-detail'
-import ReqestConsumptionPage from '../../omg-page-consumption'
-import TransactionRequestPage from '../../omg-page-transaction-request'
-import TransactionExportPage from '../../omg-page-transaction-export'
-import ConfigurationPage from '../../omg-page-configuration'
-import AdminsPage from '../../omg-page-admins'
-import ActivityLogPage from '../../omg-page-activity-log'
-import AdminDetailPage from '../../omg-page-admin-detail'
-import NotFoundPage from '../../omg-page-404'
-import AccountLayout from '../../omg-page-each-account/AccountLayout'
-import VerifyEmail from '../../omg-page-verify-email'
-import ModalController from '../../omg-modal/ModalController'
-import Alert from '../../omg-alert'
+import { connect } from 'react-redux'
+
+import history from 'adminPanelApp/routes/history'
+import AuthenticatedRoute from 'adminPanelApp/routes/authenticatedRoute'
+import LoginRoute from 'adminPanelApp/routes/loginRoute'
+import LoginForm from 'omg-login-form'
+import ForgetPasswordForm from 'omg-forgetPassword-form'
+import CreateNewPasswordForm from 'omg-createNewPassword-form'
+import InviteForm from 'omg-invite-form'
+import AccountPage from 'omg-page-account'
+import TokenPage from 'omg-page-token'
+import TransactionPage from 'omg-page-transaction'
+import WalletPage from 'omg-page-wallets'
+import BlockchainWalletDetailPage from 'omg-page-blockchain-wallet-detail'
+import BlockchainWalletsPage from 'omg-page-blockchain-wallets'
+import UserSettingPage from 'omg-page-user-setting'
+import ApiKeyPage from 'omg-page-api'
+import ApiKeyDetailPage from 'omg-page-api-detail'
+import KeyDetailAccountsPage from 'omg-page-api-detail-accounts'
+import UserPage from 'omg-page-users'
+import TokenDetailPage from 'omg-page-token-detail'
+import WalletDetailPage from 'omg-page-wallet-detail'
+import UserDetailPage from 'omg-page-user-detail'
+import ReqestConsumptionPage from 'omg-page-consumption'
+import TransactionRequestPage from 'omg-page-transaction-request'
+import TransactionExportPage from 'omg-page-transaction-export'
+import ConfigurationPage from 'omg-page-configuration'
+import AdminsPage from 'omg-page-admins'
+import ActivityLogPage from 'omg-page-activity-log'
+import AdminDetailPage from 'omg-page-admin-detail'
+import NotFoundPage from 'omg-page-404'
+import AccountLayout from 'omg-page-each-account/AccountLayout'
+import VerifyEmail from 'omg-page-verify-email'
+import ModalController from 'omg-modal/ModalController'
+import Alert from 'omg-alert'
+import { selectBlockchainEnabled } from 'omg-configuration/selector'
+
 // prettier-ignore
 
-const createRoute = () => (
+const createRoute = ({ blockchainEnabled }) => (
   <Router basename='/admin' history={history}>
     <Switch>
       <Redirect from='/' to={'/accounts'} exact />
@@ -51,7 +55,7 @@ const createRoute = () => (
       <AuthenticatedRoute path='/tokens' exact component={TokenPage} />
       <AuthenticatedRoute path='/tokens/:viewTokenId/:state' exact component={TokenDetailPage} />
       <AuthenticatedRoute path='/tokens/:viewTokenId' exact component={TokenDetailPage} />
-      <AuthenticatedRoute path='/wallets' exact component={WalletPage} />
+      <AuthenticatedRoute path='/wallets' exact component={blockchainEnabled? NotFoundPage : WalletPage} />
       <AuthenticatedRoute path='/blockchain_wallets' exact component={BlockchainWalletsPage} />
       <AuthenticatedRoute path='/blockchain_wallets/:address' component={BlockchainWalletDetailPage} />
       <AuthenticatedRoute path='/transaction' exact component={TransactionPage} />
@@ -73,14 +77,14 @@ const createRoute = () => (
       <AuthenticatedRoute path='/users/:userId/:type' exact component={() => <UserDetailPage withBreadCrumb />} />
       <AuthenticatedRoute path='/users/:userId' exact component={() => <UserDetailPage withBreadCrumb />} />
 
-      <AuthenticatedRoute path='/consumptions' exact component={ReqestConsumptionPage} />
-      <AuthenticatedRoute path='/requests' exact component={TransactionRequestPage} />
+      <AuthenticatedRoute path='/consumptions' exact component={blockchainEnabled? NotFoundPage : ReqestConsumptionPage} />
+      <AuthenticatedRoute path='/requests' exact component={blockchainEnabled? NotFoundPage : TransactionRequestPage} />
       <AuthenticatedRoute path='/activity' exact component={ActivityLogPage} />
       <AuthenticatedRoute path='/users' exact component={UserPage} />
       <AuthenticatedRoute path='/admins' exact component={AdminsPage} />
       <AuthenticatedRoute path='/admins/:adminId' exact component={AdminDetailPage} />
-      <AuthenticatedRoute path='/wallets/:walletAddress' exact component={WalletDetailPage} />
-      <AuthenticatedRoute path='/wallets/:walletAddress/:type' exact component={WalletDetailPage} />
+      <AuthenticatedRoute path='/wallets/:walletAddress' exact component={blockchainEnabled? NotFoundPage : WalletDetailPage} />
+      <AuthenticatedRoute path='/wallets/:walletAddress/:type' exact component={blockchainEnabled? NotFoundPage : WalletDetailPage} />
 
       {/* 404 PAGE */}
       <Route component={NotFoundPage} />
@@ -90,4 +94,8 @@ const createRoute = () => (
   </Router>
 )
 
-export default createRoute
+const mapStateToProps = (state) => ({
+  blockchainEnabled: selectBlockchainEnabled()(state)
+})
+
+export default  connect(mapStateToProps, null)(createRoute)
