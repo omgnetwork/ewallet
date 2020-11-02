059dbcd082ef5875c26b6b0c6ddfe2be9a6c7a7b
Mederic <mederic@omise.co>
2020-11-02 10:23:23 +0700

    test fix

--- .circleci/ci_e2e.sh
+++ .circleci/ci_e2e.sh
@@ -85,8 +85,7 @@ docker-compose run --rm ewallet config gcs_credentials "$unescaped_gcs_creds" >/
 ## Running E2E
 ##
 
-# _merge_base="master"
-_merge_base="mederic-p/fix-e2e"
+_merge_base="master"
 _branches="$(git ls-remote -h --refs git@github.com:omgnetwork/ewallet 'v*' | awk '{ print $2 }' | sort -r)"
 
 for _branch in "refs/heads/master" $_branches; do
